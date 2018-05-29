fe318515e1ef3a09ca3b0ad3f59cdf90a6c6e1a5
Deric Walintukan <deric.walintukan@gmail.com>
2018-05-29 18:13:40 +0700

    Init files

---
+++ src/localization/i18n.js
@@ -0,0 +1,36 @@
+const _ = require('lodash');
+const { app } = require('electron');
+
+const { getLogger } = require('../utils/logger');
+const locale = require('./locale.json');
+
+class i18n {
+  constructor() {
+    // Set locale based on OS language
+    let lang = 'zh';
+    const osLang = app.getLocale();
+    if (osLang) {
+      if (osLang.startsWith('en')) {
+        lang = 'en';
+      } else if (osLang.startsWith('zh')) {
+        lang = 'zh';
+      }
+    }
+
+    this.lang = lang;
+  }
+
+  get(strName) {
+    const str = locale[strName][this.lang];
+    if (!_.isEmpty(str)) {
+      return str;
+    }
+
+    // Could not find translated string, default to English
+    return locale[strName].en;
+  }
+}
+
+const instance = new i18n();
+Object.freeze(instance);
+module.exports = instance;
