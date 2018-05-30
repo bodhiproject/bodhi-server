ff8a60b311ef44e5d32c1dca9b33966a9e9882d3
Deric Walintukan <deric.walintukan@gmail.com>
2018-05-30 11:52:45 +0700

    Rearrange utils methods

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -10,6 +10,82 @@ const { getLogger } = require('./logger');
 
 const DIR_DEV = 'dev';
 
+// Checks for dev flag
+function isDevEnv() {
+  return _.includes(process.argv, '--dev');
+}
+
+// Returns the path where the data directory is, and also creates the directory if it doesn't exist.
+function getBaseDataDir() {
+  const osDataDir = app.getPath('userData');
+  const pathPrefix = isMainnet() ? 'mainnet' : 'testnet';
+  let basePath = `${osDataDir}/${pathPrefix}`;
+  if (isDevEnv()) {
+    basePath += '/dev';
+  }
+  return basePath;
+}
+
+// Returns the path where the blockchain version directory is.
+function getVersionDir() {
+  const basePath = getBaseDataDir();
+  const regex = RegExp(/(\d+)\.(\d+)\.(\d+)-(c\d+)-(d\d+)/g);
+  const regexGroups = regex.exec(version);
+  if (regexGroups === null) {
+    throw new Error(`Invalid version number: ${version}`);
+  }
+
+  // Example: 0.6.5-c0-d1
+  // c0 = contract version 0, d1 = db version 1
+  const versionDir = `${basePath}/${regexGroups[4]}_${regexGroups[5]}`; // c0_d1
+
+  // Create data dir if needed
+  fs.ensureDirSync(versionDir);
+
+  return versionDir;
+}
+
+/*
+* Returns the path where the blockchain data directory is, and also creates the directory if it doesn't exist.
+*/
+function getDataDir() {
+  const versionDir = getVersionDir();
+
+  // production
+  const dataDir = `${versionDir}/nedb`;
+
+  // Create data dir if needed
+  fs.ensureDirSync(dataDir);
+
+  return dataDir;
+}
+
+/*
+* Returns the path where the blockchain log directory is, and also creates the directory if it doesn't exist.
+*/
+function getLogDir() {
+  const versionDir = getVersionDir();
+  const logDir = `${versionDir}/logs`;
+
+  // Create data dir if needed
+  fs.ensureDirSync(logDir);
+
+  return logDir;
+}
+
+/*
+* Returns the path where the local cache data (Transaction table) directory is, and also creates the directory if it doesn't exist.
+* The Local cache should exist regardless of version change, for now
+*/
+function getLocalCacheDataDir() {
+  const dataDir = `${getBaseDataDir()}/local/nedb`;
+
+  // Create data dir if needed
+  fs.ensureDirSync(dataDir);
+
+  return dataDir;
+}
+
 /*
 * Gets the dev env qtum path for either qtumd or qtum-qt.
 * @param exec {String} The exec file type needed to be returned.
@@ -135,42 +211,14 @@ function getProdQtumPath(exec) {
   return path.replace('app.asar', 'app.asar.unpacked');
 }
 
-function isDevEnv() {
-  return _.includes(process.argv, '--dev');
-}
-
-/*
-* Returns the path where the data directory is, and also creates the directory if it doesn't exist.
-*/
-function getBaseDataDir() {
-  const osDataDir = app.getPath('userData');
-  const pathPrefix = isMainnet() ? 'mainnet' : 'testnet';
-  let basePath = `${osDataDir}/${pathPrefix}`;
+function getQtumPath(exec) {
+  let qtumPath;
   if (isDevEnv()) {
-    basePath += '/dev';
+    qtumPath = getDevQtumPath(exec);
+  } else {
+    qtumPath = getProdQtumPath(exec);
   }
-  return basePath;
-}
-
-/*
-* Returns the path where the blockchain version directory is.
-*/
-function getVersionDir() {
-  const basePath = getBaseDataDir();
-  const regex = RegExp(/(\d+)\.(\d+)\.(\d+)-(c\d+)-(d\d+)/g);
-  const regexGroups = regex.exec(version);
-  if (regexGroups === null) {
-    throw new Error(`Invalid version number: ${version}`);
-  }
-
-  // Example: 0.6.5-c0-d1
-  // c0 = contract version 0, d1 = db version 1
-  const versionDir = `${basePath}/${regexGroups[4]}_${regexGroups[5]}`; // c0_d1
-
-  // Create data dir if needed
-  fs.ensureDirSync(versionDir);
-
-  return versionDir;
+  return qtumPath;
 }
 
 /*
@@ -193,98 +241,54 @@ function hexToDecimalString(input) {
   return input.toString();
 }
 
+function hexArrayToDecimalArray(array) {
+  if (!array) {
+    return undefined;
+  }
+  return _.map(array, item => hexToDecimalString(item));
+}
+
+async function isAllowanceEnough(owner, spender, amount) {
+  try {
+    const res = await require('../api/bodhi_token').allowance({
+      owner,
+      spender,
+      senderAddress: owner,
+    });
+
+    const allowance = Web3Utils.toBN(res.remaining);
+    const amountBN = Web3Utils.toBN(amount);
+    return allowance.gte(amountBN);
+  } catch (err) {
+    getLogger().error(`Error checking allowance: ${err.message}`);
+    throw err;
+  }
+}
+
+// Get correct gas limit determined if voting over consensus threshold or not
+async function getVotingGasLimit(oraclesDb, oracleAddress, voteOptionIdx, voteAmount) {
+  const oracle = await oraclesDb.findOne({ address: oracleAddress }, { consensusThreshold: 1, amounts: 1 });
+  if (!oracle) {
+    getLogger().error(`Could not find Oracle ${oracleAddress} in DB.`);
+    throw new Error(`Could not find Oracle ${oracleAddress} in DB.`);
+  }
+
+  const threshold = Web3Utils.toBN(oracle.consensusThreshold);
+  const currentTotal = Web3Utils.toBN(oracle.amounts[voteOptionIdx]);
+  const maxVote = threshold.sub(currentTotal);
+  return Web3Utils.toBN(voteAmount).gte(maxVote) ? Config.CREATE_DORACLE_GAS_LIMIT : Config.DEFAULT_GAS_LIMIT;
+}
+
 module.exports = {
   isDevEnv,
   getBaseDataDir,
   getVersionDir,
+  getDataDir,
+  getLogDir,
+  getLocalCacheDataDir,
+  getQtumPath,
   hexToDecimalString,
-
-  getQtumPath: (exec) => {
-    let qtumPath;
-    if (isDevEnv()) {
-      qtumPath = getDevQtumPath(exec);
-    } else {
-      qtumPath = getProdQtumPath(exec);
-    }
-    return qtumPath;
-  },
-
-  /*
-  * Returns the path where the blockchain data directory is, and also creates the directory if it doesn't exist.
-  */
-  getDataDir: () => {
-    const versionDir = getVersionDir();
-
-    // production
-    const dataDir = `${versionDir}/nedb`;
-
-    // Create data dir if needed
-    fs.ensureDirSync(dataDir);
-
-    return dataDir;
-  },
-
-  /*
-  * Returns the path where the blockchain log directory is, and also creates the directory if it doesn't exist.
-  */
-  getLogDir: () => {
-    const versionDir = getVersionDir();
-    const logDir = `${versionDir}/logs`;
-
-    // Create data dir if needed
-    fs.ensureDirSync(logDir);
-
-    return logDir;
-  },
-
-  /*
-  * Returns the path where the local cache data (Transaction table) directory is, and also creates the directory if it doesn't exist.
-  * The Local cache should exist regardless of version change, for now
-  */
-  getLocalCacheDataDir: () => {
-    const dataDir = `${getBaseDataDir()}/local/nedb`;
-
-    // Create data dir if needed
-    fs.ensureDirSync(dataDir);
-
-    return dataDir;
-  },
-
-  hexArrayToDecimalArray: (array) => {
-    if (!array) {
-      return undefined;
-    }
-    return _.map(array, item => hexToDecimalString(item));
-  },
-
-  isAllowanceEnough: async (owner, spender, amount) => {
-    try {
-      const res = await require('../api/bodhi_token').allowance({
-        owner,
-        spender,
-        senderAddress: owner,
-      });
-
-      const allowance = Web3Utils.toBN(res.remaining);
-      const amountBN = Web3Utils.toBN(amount);
-      return allowance.gte(amountBN);
-    } catch (err) {
-      getLogger().error(`Error checking allowance: ${err.message}`);
-      throw err;
-    }
-  },
-
-  // Get correct gas limit determined if voting over consensus threshold or not
-  getVotingGasLimit: async (oraclesDb, oracleAddress, voteOptionIdx, voteAmount) => {
-    const oracle = await oraclesDb.findOne({ address: oracleAddress }, { consensusThreshold: 1, amounts: 1 });
-    if (!oracle) {
-      getLogger().error(`Could not find Oracle ${oracleAddress} in DB.`);
-      throw new Error(`Could not find Oracle ${oracleAddress} in DB.`);
-    }
-
-    const threshold = Web3Utils.toBN(oracle.consensusThreshold);
-    const currentTotal = Web3Utils.toBN(oracle.amounts[voteOptionIdx]);
-    const maxVote = threshold.sub(currentTotal);
-    return Web3Utils.toBN(voteAmount).gte(maxVote) ? Config.CREATE_DORACLE_GAS_LIMIT : Config.DEFAULT_GAS_LIMIT;
-  },
+  hexArrayToDecimalArray,
+  isAllowanceEnough,
+  getVotingGasLimit,
 };
