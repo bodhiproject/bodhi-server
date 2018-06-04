d80c6fc1374a43a3187eb7585543beef5f1430cc
Deric Walintukan <deric.walintukan@gmail.com>
2018-06-04 23:27:04 +0700

    Refactor export for method

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ const _ = require('lodash');
 
 const { startServer } = require('./server');
 const { blockchainEnv, execFile } = require('./constants');
-const Utils = require('./utils/utils');
+const { getQtumPath } = require('./utils/utils');
 
 if (_.includes(process.argv, '--testnet')) {
-  startServer(blockchainEnv.TESTNET, Utils.getQtumPath(execFile.QTUMD));
+  startServer(blockchainEnv.TESTNET, getQtumPath(execFile.QTUMD));
 } else if (_.includes(process.argv, '--mainnet')) {
-  startServer(blockchainEnv.MAINNET, Utils.getQtumPath(execFile.QTUMD));
+  startServer(blockchainEnv.MAINNET, getQtumPath(execFile.QTUMD));
 } else {
   throw Error('testnet or mainnet flag not found.');
 }
