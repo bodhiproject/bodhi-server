7a7a2fdad0a4459e7005c5a6fed3d9dc55f587e7
Deric Walintukan <deric.walintukan@gmail.com>
2018-06-04 15:40:20 +0700

    Pass right flag

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const _ = require('lodash');
 
 const { startServer } = require('./server');
-const { blockchainEnv } = require('./constants');
+const { blockchainEnv, execFile } = require('./constants');
 const Utils = require('./utils/utils');
 
 if (_.includes(process.argv, '--testnet')) {
-  startServer(blockchainEnv.TESTNET, Utils.getQtumPath(blockchainEnv.TESTNET));
+  startServer(blockchainEnv.TESTNET, Utils.getQtumPath(execFile.QTUMD));
 } else if (_.includes(process.argv, '--mainnet')) {
-  startServer(blockchainEnv.MAINNET, Utils.getQtumPath(blockchainEnv.MAINNET));
+  startServer(blockchainEnv.MAINNET, Utils.getQtumPath(execFile.QTUMD));
 } else {
   throw Error('testnet or mainnet flag not found.');
 }
