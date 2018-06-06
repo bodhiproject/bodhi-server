d6565af792a519690cbaed7dae01326c6a9aea32
Deric Walintukan <deric.walintukan@gmail.com>
2018-06-07 00:40:13 +0700

    Change loglvl (#22)
    * Change default loglevel
    
    * Update version
    
    * Change loglvl key to all uppercase

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -51,7 +51,7 @@ function initLogger() {
     }
 
     logger = new (winston.Logger)({ transports, exitOnError: false });
-    logger.level = process.env.loglvl || Config.DEFAULT_LOGLVL;
+    logger.level = process.env.LOG_LEVEL || Config.DEFAULT_LOGLVL;
     logger.info(`Logs path: ${logDir}`);
   }
 }
