abc1c56655728e7562db4161b69207e077bb7f9d
Deric Walintukan <deric.walintukan@gmail.com>
2018-06-15 17:24:55 +0700

    Add sync coracle

--- src/sync/sync.js
+++ src/sync/sync.js
@@ -27,8 +27,7 @@ const syncTopicCreated = async (currentBlockNum) => {
       [contractMetadata.EventFactory.TopicCreated], contractMetadata, REMOVE_HEX_PREFIX);
     getLogger().debug(`${result.length} TopicCreated entries`);
   } catch (err) {
-    getLogger().error(`searchlog TopicCreated: ${err.message}`);
-    return;
+    throw Error(`searchlog TopicCreated: ${err.message}`);
   }
 
   const topicEventPromises = [];
@@ -38,7 +37,7 @@ const syncTopicCreated = async (currentBlockNum) => {
 
     _.forEachRight(event.log, (entry) => {
       if (entry._eventName === 'TopicCreated') {
-        const insertTopicPromise = new Promise(async (resolve) => {
+        topicEventPromises.push(new Promise(async (resolve) => {
           try {
             const topic = new Topic(blockNum, txid, entry).translate();
 
@@ -54,9 +53,7 @@ const syncTopicCreated = async (currentBlockNum) => {
             getLogger().error(`insert TopicEvent: ${err.message}`);
             resolve();
           }
-        });
-
-        topicEventPromises.push(insertTopicPromise);
+        }));
       }
     });
   });
@@ -64,10 +61,56 @@ const syncTopicCreated = async (currentBlockNum) => {
   await Promise.all(topicEventPromises);
 };
 
+const syncCentralizedOracleCreated = async (currentBlockNum) => {
+  let result;
+  try {
+    result = await getInstance().searchLogs(currentBlockNum, currentBlockNum, contractMetadata.EventFactory.address,
+      [contractMetadata.OracleFactory.CentralizedOracleCreated], contractMetadata, REMOVE_HEX_PREFIX);
+    getLogger().debug(`${result.length} CentralizedOracleCreated entries`);
+  } catch (err) {
+    throw Error(`searchlog CentralizedOracleCreated: ${err.message}`);
+  }
+  
+  const cOraclePromises = [];
+  _.forEach(result, (event, index) => {
+    const blockNum = event.blockNumber;
+    const txid = event.transactionHash;
+
+    _.forEachRight(event.log, (rawLog) => {
+      if (rawLog._eventName === 'CentralizedOracleCreated') {
+        cOraclePromises.push(new Promise(async (resolve) => {
+          try {
+            const cOracle = new CentralizedOracle(blockNum, txid, rawLog).translate();
+
+            // Insert existing Topic info into Oracle
+            const topic = await DBHelper.findOne(db.Topics, { address: cOracle.topicAddress }, ['name', 'options']);
+            centralOracle.name = topic.name;
+            centralOracle.options = topic.options;
+
+            // Update existing mutated Oracle or insert new
+            if (await DBHelper.getCount(db.Oracles, { txid }) > 0) {
+              DBHelper.updateOracleByQuery(db.Oracles, { txid }, centralOracle);
+            } else {
+              DBHelper.insertOracle(db.Oracles, centralOracle);
+            }
+
+            resolve();
+          } catch (err) {
+            getLogger().error(`insert CentralizedOracle: ${err.message}`);
+            resolve();
+          }
+        }));
+      }
+    });
+  });
+
+  await Promise.all(cOraclePromises);
+};
+
 const sync = async (blockNum) => {
   contractMetadata = getContractMetadata();
 
   getLogger().debug(`Syncing blockNum ${blockNum}`);
-
   await syncTopicCreated(blockNum);
+  await syncCentralizedOracleCreated(blockNum);
 };
