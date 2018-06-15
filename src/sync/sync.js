837e6645d10091b14b3bf3c739ebb1ed4a12a8f4
Deric Walintukan <deric.walintukan@gmail.com>
2018-06-15 17:16:35 +0700

    Add sync topic created

--- src/sync/sync.js
+++ src/sync/sync.js
@@ -1,12 +1,9 @@
 const { getContractMetadata, isMainnet } = require('../config');
 const { db, DBHelper } = require('../db/nedb');
+const { getLogger } = require('../utils/logger');
 
+const REMOVE_HEX_PREFIX = true;
 let contractMetadata;
-
-const sync = async () => {
-  contractMetadata = getContractMetadata();
-  const startBlock = await getStartBlock();
-};
 
 // Returns the starting block to start syncing
 const getStartBlock = async () => {
@@ -21,4 +18,56 @@ const getStartBlock = async () => {
   }
 
   return startBlock;
+};
+
+const syncTopicCreated = async (currentBlockNum) => {
+  let result;
+  try {
+    result = await getInstance().searchLogs(currentBlockNum, currentBlockNum, contractMetadata.EventFactory.address,
+      [contractMetadata.EventFactory.TopicCreated], contractMetadata, REMOVE_HEX_PREFIX);
+    getLogger().debug(`${result.length} TopicCreated entries`);
+  } catch (err) {
+    getLogger().error(`searchlog TopicCreated: ${err.message}`);
+    return;
+  }
+
+  const topicEventPromises = [];
+  _.forEach(result, (event, index) => {
+    const blockNum = event.blockNumber;
+    const txid = event.transactionHash;
+
+    _.forEachRight(event.log, (entry) => {
+      if (entry._eventName === 'TopicCreated') {
+        const insertTopicPromise = new Promise(async (resolve) => {
+          try {
+            const topic = new Topic(blockNum, txid, entry).translate();
+
+            // Update existing mutated Topic or insert new
+            if (await DBHelper.getCount(db.Topics, { txid }) > 0) {
+              DBHelper.updateTopicByQuery(db.Topics, { txid }, topic);
+            } else {
+              DBHelper.insertTopic(db.Topics, topic);
+            }
+
+            resolve();
+          } catch (err) {
+            getLogger().error(`insert TopicEvent: ${err.message}`);
+            resolve();
+          }
+        });
+
+        topicEventPromises.push(insertTopicPromise);
+      }
+    });
+  });
+
+  await Promise.all(topicEventPromises);
+};
+
+const sync = async (blockNum) => {
+  contractMetadata = getContractMetadata();
+
+  getLogger().debug(`Syncing blockNum ${blockNum}`);
+
+  await syncTopicCreated(blockNum);
 };
