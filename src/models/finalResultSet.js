732c348e56c8ac1f76cd9e981c0acfc917c54aaf
Deric Walintukan <deric.walintukan@gmail.com>
2018-06-18 21:30:29 +0700

    Add more validation checks to models

--- src/models/finalResultSet.js
+++ src/models/finalResultSet.js
@@ -6,13 +6,24 @@ const { isMainnet } = require('../config');
 
 class FinalResultSet {
   constructor(blockNum, txid, fromAddress, rawLog) {
-    if (!_.isEmpty(rawLog)) {
-      this.blockNum = blockNum;
-      this.txid = txid;
-      this.fromAddress = Decoder.toQtumAddress(fromAddress, isMainnet());
-      this.rawLog = rawLog;
-      this.decode();
+    if (!_.isFinite(blockNum)) {
+      throw Error('blockNum must be a Number');
     }
+    if (!_.isString(txid)) {
+      throw Error('txid must be a String');
+    }
+    if (!_.isString(fromAddress)) {
+      throw Error('fromAddress must be a String');
+    }
+    if (_.isEmpty(rawLog)) {
+      throw Error('rawLog must not be empty');
+    }
+
+    this.blockNum = blockNum;
+    this.txid = txid;
+    this.fromAddress = Decoder.toQtumAddress(fromAddress, isMainnet());
+    this.rawLog = rawLog;
+    this.decode();
   }
 
   decode() {
