ad63213c35ef72928157f3558a61bd5f4e947082
Deric Walintukan <deric.walintukan@gmail.com>
2018-06-20 18:44:15 +0700

    Remove param from get qtum dev exec path

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const _ = require('lodash');
 
 const { startServer } = require('./server');
-const { blockchainEnv, execFile } = require('./constants');
+const { blockchainEnv } = require('./constants');
 const { getDevQtumExecPath } = require('./utils');
 
 if (_.includes(process.argv, '--testnet')) {
-  startServer(blockchainEnv.TESTNET, getDevQtumExecPath(execFile.QTUMD));
+  startServer(blockchainEnv.TESTNET, getDevQtumExecPath());
 } else if (_.includes(process.argv, '--mainnet')) {
-  startServer(blockchainEnv.MAINNET, getDevQtumExecPath(execFile.QTUMD));
+  startServer(blockchainEnv.MAINNET, getDevQtumExecPath());
 } else {
   throw Error('testnet or mainnet flag not found.');
 }
