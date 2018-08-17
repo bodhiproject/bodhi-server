aedb7c3b04f8e478e44df04c565ca90f4ea0bbee
Deric Walintukan <deric.walintukan@gmail.com>
2018-08-17 17:14:06 +0700

    Cron job (#73)
    * Add script for renewing
    
    * Add cp command for renew script
    
    * Adjust comments
    
    * Change permissions
    
    * Add checks to for some script actions
    
    * Remove else statement
    
    * Readd if statement
    
    * Add then statements
    
    * Add missing semicolons
    
    * Add setup python script
    
    * Fix missing src
    
    * Fix bad sites dir
    
    * Fix bad ref
    
    * Fix join call
    
    * Add check for symlink existance
    
    * Fix syntax
    
    * Add not operator
    
    * Remove shell script
    
    * Add readme
    
    * Change text
    
    * Add more instructions
    
    * Fix md
    
    * Fix  comments

---
+++ scripts/setup.py
@@ -0,0 +1,40 @@
+import os
+import os.path
+import shutil
+from subprocess import call
+
+def fileExists(path):
+    return os.path.isfile(path) and os.access(path, os.R_OK)
+
+# remove the default sites-available config
+path = '/etc/nginx/sites-available/default'
+if fileExists(path):
+    print 'Removing default sites-available...'
+    os.remove(path)
+
+# remove the default sites-enabled config
+path = '/etc/nginx/sites-enabled/default'
+if fileExists(path):
+    print 'Removing default sites-enabled...'
+    os.remove(path)
+
+# copy nginx server block files to sites-available
+sites_dir = './nginx-sites'
+for filename in os.listdir(sites_dir):
+    full_filename = os.path.join(sites_dir, filename)
+    if (os.path.isfile(full_filename)):
+        shutil.copy(full_filename, '/etc/nginx/sites-available/')
+
+# add symlink to sites-available
+path = '/etc/nginx/sites-enabled/node'
+if not fileExists(path):
+    print 'Adding symlinks...'
+    call(['sudo', 'ln', '-s', '/etc/nginx/sites-available/node', '/etc/nginx/sites-enabled/node'])
+
+# restart nginx
+print 'Restarting nginx...'
+call(['sudo', 'service', 'nginx', 'restart'])
+
+# add cert renew script to daily cronjob
+print 'Copying cert renew script to cron.daily...'
+shutil.copy('renew-cert.sh', '/etc/cron.daily')
