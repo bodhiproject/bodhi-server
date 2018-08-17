8c92c7df34eec4e8f8acda13ce370a6b139fb128
Deric Walintukan <deric.walintukan@gmail.com>
2018-08-17 13:11:17 +0700

    Add logic for script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,8 +4,21 @@
 # It will install nginx and setup the port routing for the UI.
 
 # install nginx
+echo 'Installing nginx...'
 sudo apt-update
 sudo apt-get install nginx
 
 # remove the default sites-enabled config
+echo 'Removing default sites-enabled...'
 sudo rm /etc/nginx/sites-enabled/default
+
+# copy sites-enabled setups to default nginx config location
+echo 'Copying sites-enabled config...'
+sudo cp -a ./nginx/sites-enabled/. /etc/nginx/sites-enabled/
+
+# add symlink to sites-available
+echo 'Adding symlinks...'
+sudo ln -s /etc/nginx/sites-available/node /etc/nginx/sites-enabled/node
+
+echo 'Restarting nginx...'
+sudo service nginx restart
