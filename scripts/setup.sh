85a01adea804e94949cf4c54e809f8ed53e18485
Deric Walintukan <deric.walintukan@gmail.com>
2018-08-17 15:33:02 +0700

    Add checks to for some script actions

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,22 +4,36 @@
 # It will install nginx and setup the port routing for the UI.
 # Meant for first-time usage.
 
-# install nginx
-echo 'Installing nginx...'
-sudo apt-get update &&
-sudo apt-get install nginx &&
+# install nginx if necessary
+if [ "$(dpkg-query -W nginx)" == "dpkg-query: no packages found matching nginx" ]
+    echo 'Installing nginx...' &&
+    sudo apt-get update &&
+    sudo apt-get install nginx &&
+else
+    echo 'nginx already installed.' &&
+fi
+
+# remove the default sites-available config
+if [ "$(ls /etc/nginx/sites-available/default)" == "/etc/nginx/sites-available/default" ]
+    echo 'Removing default sites-available...' &&
+    sudo rm /etc/nginx/sites-available/default &&
+fi
 
 # remove the default sites-enabled config
-echo 'Removing default sites-enabled...' &&
-sudo rm /etc/nginx/sites-enabled/default &
+if [ "$(ls /etc/nginx/sites-enabled/default)" == "/etc/nginx/sites-enabled/default" ]
+    echo 'Removing default sites-enabled...' &&
+    sudo rm /etc/nginx/sites-enabled/default &&
+fi
 
-# copy nginx config to sites-available
+# copy nginx server block files to sites-available
 echo 'Copying sites-enabled config...' &&
-sudo cp -a ./nginx-sites/. /etc/nginx/sites-available/ &
+sudo cp -a ./nginx-sites/. /etc/nginx/sites-available/ &&
 
 # add symlink to sites-available
-echo 'Adding symlinks...' &&
-sudo ln -s /etc/nginx/sites-available/node /etc/nginx/sites-enabled/node &
+if [ "$(ls /etc/nginx/sites-enabled/node)" == "ls: cannot access '/etc/nginx/sites-enabled/node': No such file or directory" ]
+    echo 'Adding symlinks...' &&
+    sudo ln -s /etc/nginx/sites-available/node /etc/nginx/sites-enabled/node &
+fi
 
 # restart nginx
 echo 'Restarting nginx...' &&
@@ -27,4 +41,4 @@ sudo service nginx restart &&
 
 # add cert renew script to daily cronjob
 echo 'Copying cert renew script to cron.daily...' &&
-sudo cp renewcert.sh /etc/cron.daily
+sudo cp renew-cert.sh /etc/cron.daily
