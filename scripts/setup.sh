46c79fa1baf7a47c3aaa303d1cbcaab73cc2e35f
Deric Walintukan <deric.walintukan@gmail.com>
2018-08-17 15:34:21 +0700

    Remove else statement

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,8 +9,6 @@ if [ "$(dpkg-query -W nginx)" == "dpkg-query: no packages found matching nginx" 
     echo 'Installing nginx...' &&
     sudo apt-get update &&
     sudo apt-get install nginx &&
-else
-    echo 'nginx already installed.' &&
 fi
 
 # remove the default sites-available config
