b689e407f8fa848d0ab4f96c6f997017a67399da
Deric Walintukan <deric.walintukan@gmail.com>
2018-08-17 16:52:38 +0700

    Fix bad ref

--- scripts/setup.py
+++ scripts/setup.py
@@ -18,7 +18,7 @@ if os.path.isfile(path) and os.access(path, os.R_OK):
 # copy nginx server block files to sites-available
 sites_dir = os.listdir('./nginx-sites')
 for filename in sites_dir:
-    full_filename = os.path.join(sitesDir, filename)
+    full_filename = os.path.join(sites_dir, filename)
     if (os.path.isfile(full_filename)):
         shutil.copy(full_filename, '/etc/nginx/sites-available/')
 
