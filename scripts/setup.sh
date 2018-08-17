870be33ec7e8b14176768b9ed159da921de72be7
Deric Walintukan <deric.walintukan@gmail.com>
2018-08-17 15:13:51 +0700

    Add cp command for renew script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,6 +2,7 @@
 
 # This script is meant for Linux-based systems only.
 # It will install nginx and setup the port routing for the UI.
+# Meant for first-time usage.
 
 # install nginx
 echo 'Installing nginx...'
@@ -10,15 +11,20 @@ sudo apt-get install nginx &&
 
 # remove the default sites-enabled config
 echo 'Removing default sites-enabled...' &&
-sudo rm /etc/nginx/sites-enabled/default &&
+sudo rm /etc/nginx/sites-enabled/default &
 
 # copy nginx config to sites-available
 echo 'Copying sites-enabled config...' &&
-sudo cp -a ./nginx-sites/. /etc/nginx/sites-available/ &&
+sudo cp -a ./nginx-sites/. /etc/nginx/sites-available/ &
 
 # add symlink to sites-available
 echo 'Adding symlinks...' &&
-sudo ln -s /etc/nginx/sites-available/node /etc/nginx/sites-enabled/node &&
+sudo ln -s /etc/nginx/sites-available/node /etc/nginx/sites-enabled/node &
 
+# restart nginx
 echo 'Restarting nginx...' &&
-sudo service nginx restart
+sudo service nginx restart &&
+
+# copy 
+echo 'Copying cert renew script to cron.daily...' &&
+sudo cp renewcert.sh /etc/cron.daily
