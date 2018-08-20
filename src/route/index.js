7e6efc5d1c9bad957d102fce0615b5d50a6d3f5a
Deric Walintukan <deric.walintukan@gmail.com>
2018-08-20 11:22:12 +0700

    Add init func to get express app

--- src/route/index.js
+++ src/route/index.js
@@ -3,7 +3,6 @@ const WebSocketServer = require('ws').Server;
 const path = require('path');
 const bodyParser = require('body-parser');
 const expressWinston = require('express-winston');
-const http = require('http');
 const helmet = require('helmet');
 
 const apiRouter = require('./api');
@@ -12,33 +11,39 @@ const { graphqlRouter, createSubscriptionServer } = require('./graphql');
 const { getLogger } = require('../utils/logger');
 const { Config } = require('../config');
 
+const initExpressApp = () => {
+  const app = express();
+
+  // Allow CORS
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next();
+  });
+
+  // Set security-related HTTPS headers
+  app.use(helmet());
+
+  // Setup for JSON and url encoded bodies
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+
+  // Route responses to Winston logger
+  app.use(expressWinston.logger({
+    winstonInstance: getLogger(),
+    meta: false,
+    msg: '{{req.method}} {{req.path}} {{res.statusCode}} {{res.body}}',
+    colorize: true,
+  }));
+
+  return app;
+};
+
 const initApiServer = () => {
   try {
-    const app = express();
-
-    // Allow CORS
-    app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-      next();
-    });
-
-    // Set security-related HTTPS headers
-    app.use(helmet());
-
-    // Setup for JSON and url encoded bodies
-    app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true }));
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-
-    // Route responses to Winston logger
-    app.use(expressWinston.logger({
-      winstonInstance: getLogger(),
-      meta: false,
-      msg: '{{req.method}} {{req.path}} {{res.statusCode}} {{res.body}}',
-      colorize: true,
-    }));
+    const app = initExpressApp();
 
     // Apply all routes
     app.use('/', apiRouter);
@@ -58,18 +63,7 @@ const initApiServer = () => {
 
 const initWebServer = () => {
   try {
-    const app = express();
-
-    // Allow CORS
-    app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-      next();
-    });
-
-    // Set security-related HTTPS headers
-    app.use(helmet());
-
+    const app = initExpressApp();
     const uiDir = path.join(__dirname, '../../node_modules/bodhi-ui/build');
     app.use(express.static(uiDir));
     app.listen(Config.PORT_HTTP, () => {
