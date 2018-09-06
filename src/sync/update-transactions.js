b02cf5e8240d93a3d1caef01c5ce6684c63d92d4
Deric Walintukan <deric.walintukan@gmail.com>
2018-09-06 12:39:59 +0700

    111 update tx fix (#112)
    * Add createdBlock for followup txs
    
    * Remove reset approve on failed tx
    
    * Remove unused
    
    * Add throws for each update tx

--- src/sync/update-transactions.js
+++ src/sync/update-transactions.js
@@ -3,7 +3,6 @@ const moment = require('moment');
 
 const Blockchain = require('../api/blockchain');
 const Wallet = require('../api/wallet');
-const BodhiToken = require('../api/bodhi-token');
 const EventFactory = require('../api/event-factory');
 const CentralizedOracle = require('../api/centralized-oracle');
 const DecentralizedOracle = require('../api/decentralized-oracle');
@@ -11,7 +10,7 @@ const Utils = require('../utils');
 const { getLogger } = require('../utils/logger');
 const { db } = require('../db');
 const DBHelper = require('../db/db-helper');
-const { Config, getContractMetadata } = require('../config');
+const { Config } = require('../config');
 const { TX_STATE, TX_TYPE, TOKEN } = require('../constants');
 const Transaction = require('../models/transaction');
 
@@ -26,11 +25,12 @@ async function updatePendingTxs(currentBlockCount) {
     pendingTxs = map(pendingTxs, tx => new Transaction(tx));
   } catch (err) {
     getLogger().error(`Get pending txs: ${err.message}`);
+    throw Error(`Get pending txs: ${err.message}`);
   }
 
   each(pendingTxs, async (tx) => {
     await updateTx(tx, currentBlockCount);
-    await updateDB(tx);
+    await updateDB(tx, currentBlockCount);
   });
 }
 
@@ -69,6 +69,7 @@ async function updateQtumTransferTx(tx, currentBlockCount) {
     }
   } catch (err) {
     getLogger().error(`updateQtumTransferTx: ${err.message}`);
+    throw Error(`updateQtumTransferTx: ${err.message}`);
   }
 }
 
@@ -94,14 +95,16 @@ async function updateEvmTx(tx) {
     tx.onConfirmed(status, blockNum, blockTime, gasUsed);
   } catch (err) {
     getLogger().error(`updateEvmTx: ${err.message}`);
+    throw Error(`updateEvmTx: ${err.message}`);
   }
 }
 
 /**
  * Update the DB with new transaction info.
  * @param {Transaction} tx Updated transaction.
+ * @param {number} currentBlockCount Current block number.
  */
-async function updateDB(tx) {
+async function updateDB(tx, currentBlockCount) {
   if (tx.status !== TX_STATE.PENDING) {
     try {
       const updateRes = await db.Transactions.update(
@@ -121,7 +124,7 @@ async function updateDB(tx) {
       if (updatedTx) {
         switch (updatedTx.status) {
           case TX_STATE.SUCCESS: {
-            await onSuccessfulTx(updatedTx);
+            await onSuccessfulTx(updatedTx, currentBlockCount);
             break;
           }
           case TX_STATE.FAIL: {
@@ -134,7 +137,8 @@ async function updateDB(tx) {
         }
       }
     } catch (err) {
-      getLogger().error(`Update Transaction ${tx.type} txid:${tx.txid} ${err.message}`);
+      getLogger().error(`updateDB: ${tx.type} txid:${tx.txid} ${err.message}`);
+      throw Error(`updateDB: ${tx.type} txid:${tx.txid} ${err.message}`);
     }
   }
 }
@@ -142,19 +146,20 @@ async function updateDB(tx) {
 /**
  * Execute follow-up transaction for successful txs.
  * @param {Transaction} tx Successful transaction.
+ * @param {number} currentBlockCount Current block number.
  */
-async function onSuccessfulTx(tx) {
+async function onSuccessfulTx(tx, currentBlockCount) {
   switch (tx.type) {
     case TX_TYPE.APPROVECREATEEVENT: {
-      await executeCreateEvent(tx);
+      await executeCreateEvent(tx, currentBlockCount);
       break;
     }
     case TX_TYPE.APPROVESETRESULT: {
-      await executeSetResult(tx);
+      await executeSetResult(tx, currentBlockCount);
       break;
     }
     case TX_TYPE.APPROVEVOTE: {
-      await executeVote(tx);
+      await executeVote(tx, currentBlockCount);
       break;
     }
     default: {
@@ -166,8 +171,9 @@ async function onSuccessfulTx(tx) {
 /**
  * The approve for a create event was accepted. Execute the create event tx.
  * @param {Transaction} tx Accepted APPROVECREATEEVENT tx.
+ * @param {number} currentBlockCount Current block number.
  */
-async function executeCreateEvent(tx) {
+async function executeCreateEvent(tx, currentBlockCount) {
   try {
     const createEventTx = await EventFactory.createTopic({
       oracleAddress: tx.resultSetterAddress,
@@ -193,6 +199,7 @@ async function executeCreateEvent(tx) {
       status: TX_STATE.PENDING,
       gasLimit: createEventTx.args.gasLimit.toString(10),
       gasPrice: createEventTx.args.gasPrice.toFixed(8),
+      createdBlock: currentBlockCount,
       createdTime: moment().unix(),
       senderAddress: tx.senderAddress,
       name: tx.name,
@@ -207,14 +214,16 @@ async function executeCreateEvent(tx) {
     });
   } catch (err) {
     getLogger().error(`executeCreateEvent: ${err.message}`);
+    throw Error(`executeCreateEvent: ${err.message}`);
   }
 }
 
 /**
  * The approve for a set result was accepted. Execute the set result tx.
  * @param {Transaction} tx Accepted APPROVESETRESULT tx.
+ * @param {number} currentBlockCount Current block number.
  */
-async function executeSetResult(tx) {
+async function executeSetResult(tx, currentBlockCount) {
   try {
     const setResultTx = await CentralizedOracle.setResult({
       contractAddress: tx.oracleAddress,
@@ -229,6 +238,7 @@ async function executeSetResult(tx) {
       status: TX_STATE.PENDING,
       gasLimit: setResultTx.args.gasLimit.toString(10),
       gasPrice: setResultTx.args.gasPrice.toFixed(8),
+      createdBlock: currentBlockCount,
       createdTime: moment().unix(),
       senderAddress: tx.senderAddress,
       topicAddress: tx.topicAddress,
@@ -239,14 +249,16 @@ async function executeSetResult(tx) {
     });
   } catch (err) {
     getLogger().error(`executeSetResult: ${err.message}`);
+    throw Error(`executeSetResult: ${err.message}`);
   }
 }
 
 /**
  * The approve for a vote was accepted. Execute the vote tx.
  * @param {Transaction} tx Accepted APPROVEVOTE tx.
+ * @param {number} currentBlockCount Current block number.
  */
-async function executeVote(tx) {
+async function executeVote(tx, currentBlockCount) {
   try {
     // Find if voting over threshold to set correct gas limit
     const gasLimit = await Utils.getVotingGasLimit(db.Oracles, tx.oracleAddress, tx.optionIdx, tx.amount);
@@ -266,6 +278,7 @@ async function executeVote(tx) {
       status: TX_STATE.PENDING,
       gasLimit: voteTx.args.gasLimit.toString(10),
       gasPrice: voteTx.args.gasPrice.toFixed(8),
+      createdBlock: currentBlockCount,
       createdTime: moment().unix(),
       senderAddress: tx.senderAddress,
       topicAddress: tx.topicAddress,
@@ -276,6 +289,7 @@ async function executeVote(tx) {
     });
   } catch (err) {
     getLogger().error(`executeVote: ${err.message}`);
+    throw Error(`executeVote: ${err.message}`);
   }
 }
 
@@ -285,58 +299,14 @@ async function executeVote(tx) {
  */
 async function onFailedTx(tx) {
   switch (tx.type) {
-    // Approve failed. Reset allowance and delete created Topic/COracle.
-    case TX_TYPE.APPROVECREATEEVENT: {
-      await resetApproveAmount(tx, getContractMetadata().AddressManager.address);
-      await removeCreatedTopicAndOracle(tx);
-      break;
-    }
-    // CreateTopic failed. Delete created Topic/COracle.
+    case TX_TYPE.APPROVECREATEEVENT:
     case TX_TYPE.CREATEEVENT: {
       await removeCreatedTopicAndOracle(tx);
-      break;
-    }
-    // Approve failed. Reset allowance.
-    case TX_TYPE.APPROVESETRESULT:
-    case TX_TYPE.APPROVEVOTE: {
-      await resetApproveAmount(tx, tx.topicAddress);
       break;
     }
     default: {
       break;
     }
-  }
-}
-
-/**
- * Resets the approve amount to 0.
- * This is a necessary step when trying to approve for an amount greater than the current approved balance.
- * @param {Transaction} tx Failed transaction.
- * @param {string} spender Address to approve.
- */
-async function resetApproveAmount(tx, spender) {
-  try {
-    const approveTx = await BodhiToken.approve({
-      spender,
-      value: 0,
-      senderAddress: tx.senderAddress,
-    });
-
-    await DBHelper.insertTransaction(db.Transactions, {
-      type: TX_TYPE.RESETAPPROVE,
-      txid: approveTx.txid,
-      status: TX_STATE.PENDING,
-      gasLimit: approveTx.args.gasLimit.toString(10),
-      gasPrice: approveTx.args.gasPrice.toFixed(8),
-      createdTime: moment().unix(),
-      version: tx.version,
-      senderAddress: tx.senderAddress,
-      topicAddress: tx.topicAddress,
-      oracleAddress: tx.oracleAddress,
-      name: tx.name,
-    });
-  } catch (err) {
-    getLogger().error(`resetApproveAmount: ${err.message}`);
   }
 }
 
@@ -350,6 +320,7 @@ async function removeCreatedTopicAndOracle(tx) {
     await DBHelper.removeOraclesByQuery(db.Oracles, { txid: tx.txid });
   } catch (err) {
     getLogger().error(`removeCreatedTopicAndOracle: ${err.message}`);
+    throw Error(`removeCreatedTopicAndOracle: ${err.message}`);
   }
 }
 
