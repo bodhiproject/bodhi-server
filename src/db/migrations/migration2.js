f85c3ae6d508fbeed83671292a9c89364f0bd59a
Zhaoyang Han <bernardhan221@gmail.com>
2018-10-24 19:01:48 -0700

    migration and unit tests finished

--- src/db/migrations/migration2.js
+++ src/db/migrations/migration2.js
@@ -1,10 +1,11 @@
-const { map } = require('lodash');
 const { getLogger } = require('../../utils/logger');
 
 async function migration2(db) {
   try {
     const votes = await db.Votes.find({ type: { $exists: false } });
-    map(votes, async (vote) => {
+    /* eslint-disable no-await-in-loop */
+    for (let i = 0; i < votes.length; i++) {
+      const vote = votes[i];
       let type;
       if (vote.token === 'QTUM') {
         type = { type: 'BET' };
@@ -14,7 +15,7 @@ async function migration2(db) {
         else if (oracle[0].token === 'BOT') type = { type: 'VOTE' };
       }
       await db.Votes.update({ txid: vote.txid }, { $set: type });
-    });
+    }
   } catch (err) {
     getLogger().error(`Migration 2 DB update Error: ${err.message}`);
     throw Error(`Migration 2 DB update Error: ${err.message}`);
--- src/db/migrations/test/migration2.tests.js
+++ src/db/migrations/test/migration2.tests.js
@@ -1,18 +1,108 @@
 const datastore = require('nedb-promise');
 const fs = require('fs');
+const { fill, map } = require('lodash');
 const fsExtra = require('fs-extra');
 const path = require('path');
 const Chai = require('chai');
 const ChaiAsPromised = require('chai-as-promised');
 const migration2 = require('../migration2');
+const DBHelper = require('../../../db/db-helper');
 
 Chai.use(ChaiAsPromised);
-const assert = Chai.assert;
 const expect = Chai.expect;
 
+async function insertOracles(db) {
+  const cOracle = {
+    blockNum: 162282,
+    txid: '6228b0ae15147dac52130c17e4fae196b36f4cde86fce3070357e724d8649f6a',
+    version: 0,
+    address: '21255aa2ece777d8d7b9572fe018761aa3a6dde1',
+    topicAddress: '64a2cc9a78f2faff6a7401e393f3a90731704ab6',
+    resultSetterAddress: 'qWG4tBKgscVxkRGwRZdBt2Whkb66VD5g9h',
+    startTime: 1529540100,
+    endTime: 1530389700,
+    resultSetStartTime: 1530389700,
+    resultSetEndTime: 1530403200,
+    consensusThreshold: '10000000000',
+    name: 'Winner of the Bay Area Best Ramen Bar Grand Prix',
+    options: ['Invalid', 'Miso-ya', 'Ramen Do-jo', 'Ippudo SF', 'Shalala Ramen'],
+    optionIdxs: [0, 1, 2, 3, 4],
+    amounts: ['0', '812000000', '0', '19990000000', '100000'],
+    resultIdx: 3,
+    status: 'WITHDRAW',
+    token: 'QTUM',
+    hashId: null,
+    language: 'zh-Hans-CN',
+  };
+
+  const dOracle = {
+    blockNum: 223343,
+    txid: 'f306b48d2197402dd801bf8a0c768f52bb5cd27b12da773bbadca680864aad64',
+    version: 0,
+    address: '596b41260670dc5d0187d0b6f92773794509c8bc',
+    topicAddress: '64a2cc9a78f2faff6a7401e393f3a90731704ab6',
+    amounts: fill(Array(5), '0'),
+    optionIdxs: [0, 1, 2, 4],
+    endTime: 1538387296,
+    consensusThreshold: '11000000000',
+    name: 'Winner of the Bay Area Best Ramen Bar Grand Prix',
+    options: ['Invalid', 'Miso-ya', 'Ramen Do-jo', 'Ippudo SF', 'Shalala Ramen'],
+    resultIdx: null,
+    startTime: 1538383696,
+    status: 'WITHDRAW',
+    token: 'BOT',
+    language: 'zh-Hans-CN',
+  };
+  await DBHelper.insertOracle(db.Oracles, cOracle);
+  await DBHelper.insertOracle(db.Oracles, dOracle);
+}
+
+async function insertVotes(db) {
+  const resultset = {
+    blockNum: 223343,
+    txid: 'f306b48d2197402dd801bf8a0c768f52bb5cd27b12da773bbadca680864aad64',
+    version: 0,
+    oracleAddress: '21255aa2ece777d8d7b9572fe018761aa3a6dde1',
+    voterAddress: 'qJHp6dUSmDShpEEMmwxqHPo7sFSdydSkPM',
+    optionIdx: 3,
+    amount: '10000000000',
+    token: 'BOT',
+    topicAddress: '64a2cc9a78f2faff6a7401e393f3a90731704ab6',
+  };
+  const vote = {
+    blockNum: 223345,
+    txid: '35d7745be0da899675ec376cc50f051a25642afc38fd3f718541f3e05638deca',
+    version: 0,
+    oracleAddress: '596b41260670dc5d0187d0b6f92773794509c8bc',
+    voterAddress: 'qJHp6dUSmDShpEEMmwxqHPo7sFSdydSkPM',
+    optionIdx: 3,
+    amount: '10000000000',
+    token: 'BOT',
+    topicAddress: '64a2cc9a78f2faff6a7401e393f3a90731704ab6',
+  };
+  const bet = {
+    blockNum: 166342,
+    txid: 'e7cef57e9202819ebbc7db919f42aaec038cbbd378c608bacb5f02d318d4021f',
+    version: 0,
+    oracleAddress: '596b41260670dc5d0187d0b6f92773794509c8bc',
+    voterAddress: 'qMmN4PNBmHvCcUAfQBHkGZsYrGkovzo8W2',
+    optionIdx: 4,
+    amount: '100000',
+    token: 'QTUM',
+    topicAddress: '64a2cc9a78f2faff6a7401e393f3a90731704ab6',
+  };
+  await db.Votes.insert(resultset);
+  await db.Votes.insert(vote);
+  await db.Votes.insert(bet);
+}
+
 async function initDB(db) {
-  db.Oracles = datastore({ filename: './mock/oracles' });
-  db.Votes = datastore({ filename: './mock/votes' });
+  if (fs.existsSync(path.join(__dirname, './mock/votes.test.db'))) fs.unlinkSync(path.join(__dirname, './mock/votes.test.db'));
+  if (fs.existsSync(path.join(__dirname, './mock/oracles.test.db'))) fs.unlinkSync(path.join(__dirname, './mock/oracles.test.db'));
+  fsExtra.ensureFileSync(path.join(__dirname, './mock/votes.test.db'));
+  fsExtra.ensureFileSync(path.join(__dirname, './mock/oracles.test.db'));
+  db.Oracles = datastore({ filename: path.join(__dirname, './mock/oracles.test.db') });
+  db.Votes = datastore({ filename: path.join(__dirname, './mock/votes.test.db') });
 
   try {
     await Promise.all([
@@ -22,6 +112,8 @@ async function initDB(db) {
 
     await db.Oracles.ensureIndex({ fieldName: 'txid', unique: true });
     await db.Votes.ensureIndex({ fieldName: 'txid', unique: true });
+    await insertOracles(db);
+    await insertVotes(db);
   } catch (err) {
     throw Error(`Migration test DB load Error: ${err.message}`);
   }
@@ -33,11 +125,22 @@ describe('Type Field Migration', () => {
       Oracles: undefined,
       Votes: undefined,
     };
-    if (fs.existsSync(path.join(__dirname, './mock/votes'))) fs.unlinkSync(path.join(__dirname, './mock/votes'));
-    fsExtra.copySync(path.join(__dirname, './mock/votes.copy'), path.join(__dirname, './mock/votes'));
+
     it('Run migration', async () => {
       await initDB(db);
       await migration2(db);
+
+      const votes = await db.Votes.find({ blockNum: { $exists: true } });
+      expect(votes.length).to.equal(3);
+      map(votes, async (vote) => {
+        if (vote.token === 'QTUM') {
+          expect(vote.type).to.equal('BET');
+        } else if (vote.token === 'BOT') {
+          const oracle = await db.Oracles.find({ address: vote.oracleAddress });
+          if (oracle[0].token === 'QTUM') expect(vote.type).to.equal('RESULT_SET');
+          else if (oracle[0].token === 'BOT') expect(vote.type).to.equal('VOTE');
+        }
+      });
     });
   });
 });
