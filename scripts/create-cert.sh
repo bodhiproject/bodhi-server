739801fc6d30d8062002b741efd83633c00aaaf6
Deric Walintukan <deric.walintukan@gmail.com>
2019-05-08 11:45:24 +0700

    Remove scripts

--- scripts/create-cert.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo 'Creating SSL certificates with Certbot...'
-certbot --nginx -d puti.io -d www.puti.io -d dev.puti.io -d test.puti.io
--- scripts/renew-cert.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Renews SSL certs with Certbot
-echo 'Renewing SSL certificates with Certbot...'
-certbot renew
