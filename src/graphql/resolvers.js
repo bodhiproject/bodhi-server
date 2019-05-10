16e3606d79ab046091f140ec4a6e2a93c67c264f
Deric Walintukan <deric.walintukan@gmail.com>
2019-05-10 18:16:34 +0700

    Add block to event resolver

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -1,25 +1,9 @@
 const { sum } = require('lodash');
 const Queries = require('./queries');
 const Mutations = require('./mutations');
-const { TX_STATUS, TOKEN, STATUS, PHASE, TX_TYPE } = require('../constants');
+const { TX_STATUS } = require('../constants');
 const pubsub = require('../route/pubsub');
 const { DBHelper } = require('../db/db-helper');
-
-/**
- * Takes an oracle object and returns which phase it is in.
- * @param {oracle} oracle
- */
-const getPhase = ({ token, status }) => {
-  const [BOT, QTUM] = [token === TOKEN.BOT, token === TOKEN.QTUM];
-
-  if (QTUM && [STATUS.VOTING, STATUS.CREATED].includes(status)) return PHASE.BETTING;
-  if (BOT && status === STATUS.VOTING) return PHASE.VOTING;
-  if (QTUM && [STATUS.WAITRESULT, STATUS.OPENRESULTSET].includes(status)) return PHASE.RESULT_SETTING;
-  if ((BOT || QTUM) && status === STATUS.PENDING) return PHASE.PENDING;
-  if (BOT && status === STATUS.WAITRESULT) return PHASE.FINALIZING;
-  if ((BOT || QTUM) && status === STATUS.WITHDRAW) return PHASE.WITHDRAWING;
-  throw Error(`Invalid Phase determined by these -> TOKEN: ${token} STATUS: ${status}`);
-};
 
 /* eslint-disable object-curly-newline */
 module.exports = {
@@ -30,6 +14,8 @@ module.exports = {
   },
 
   MultipleResultsEvent: {
+    block: async ({ blockNum }, data, { db: { Blocks } }) =>
+      (await Blocks.find({ blockNum }))[0],
     pendingTxs: async ({ address }, { pendingTxsAddress }, { db }) => {
       if (pendingTxsAddress) {
         const bet = await DBHelper.countBet(db, {
@@ -58,16 +44,19 @@ module.exports = {
     },
   },
 
-  Vote: {
-    block: async ({ blockNum }, data, { db: { Blocks } }) => (await Blocks.find({ blockNum }))[0],
+  Bet: {
+    block: async ({ blockNum }, data, { db: { Blocks } }) =>
+      (await Blocks.find({ blockNum }))[0],
   },
 
   ResultSet: {
-    block: async ({ blockNum }, data, { db: { Blocks } }) => (await Blocks.find({ blockNum }))[0],
+    block: async ({ blockNum }, data, { db: { Blocks } }) =>
+      (await Blocks.find({ blockNum }))[0],
   },
 
   Withdraw: {
-    block: async ({ blockNum }, data, { db: { Blocks } }) => (await Blocks.find({ blockNum }))[0],
+    block: async ({ blockNum }, data, { db: { Blocks } }) =>
+      (await Blocks.find({ blockNum }))[0],
   },
 };
 /* eslint-enable object-curly-newline */
