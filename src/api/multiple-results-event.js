fc02426cad246288ccdda5c46f7c49c6c29a7b38
Deric Walintukan <deric.walintukan@gmail.com>
2019-05-10 10:10:34 +0700

    Add all event apis

--- src/api/multiple-results-event.js
+++ src/api/multiple-results-event.js
@@ -1,189 +1,188 @@
-const _ = require('lodash');
-const { Contract } = require('qweb3');
+const { isUndefined, map } = require('lodash');
 
-const { getContractMetadata, getQtumRPCAddress } = require('../config');
-const Utils = require('../utils');
+const { getContractMetadata } = require('../config');
+const { web3 } = require('../web3');
+const { getLogger } = require('../utils/logger');
 
-function getContract(contractAddress) {
-  const metadata = getContractMetadata();
-  return new Contract(getQtumRPCAddress(), contractAddress, metadata.TopicEvent.abi);
+function getContract(eventAddress) {
+  const metadata = getContractMetadata().MultipleResultsEvent;
+  const naka = web3();
+  return new naka.eth.Contract(metadata.abi, eventAddress);
 }
 
-const TopicEvent = {
-  async withdrawWinnings(args) {
-    const {
-      contractAddress, // address
-      senderAddress, // address
-    } = args;
+module.exports = {
+  async calculateWinnings(args) {
+    try {
+      const { eventAddress, address } = args;
+      if (isUndefined(eventAddress)) throw TypeError('eventAddress is not defined');
+      if (isUndefined(address)) throw TypeError('address is not defined');
 
-    if (_.isUndefined(contractAddress)) {
-      throw new TypeError('contractAddress needs to be defined');
+      const res = await getContract(eventAddress).methods.calculateWinnings().call();
+      return res.toString(10);
+    } catch (err) {
+      getLogger().error(`Error MultipleResultsEvent.calculateWinnings(): ${err.message}`);
+      throw err;
     }
-    if (_.isUndefined(senderAddress)) {
-      throw new TypeError('senderAddress needs to be defined');
-    }
-
-    const contract = getContract(contractAddress);
-    return contract.send('withdrawWinnings', {
-      methodArgs: [],
-      senderAddress,
-    });
   },
 
-  async withdrawEscrow(args) {
-    const {
-      contractAddress, // address
-      senderAddress, // address
-    } = args;
+  async version(args) {
+    try {
+      const { eventAddress } = args;
+      if (isUndefined(eventAddress)) throw TypeError('eventAddress is not defined');
 
-    if (_.isUndefined(contractAddress)) {
-      throw new TypeError('contractAddress needs to be defined');
+      const res = await getContract(eventAddress).methods.version().call();
+      return res.toNumber();
+    } catch (err) {
+      getLogger().error(`Error MultipleResultsEvent.version(): ${err.message}`);
+      throw err;
     }
-    if (_.isUndefined(senderAddress)) {
-      throw new TypeError('senderAddress needs to be defined');
-    }
-
-    const contract = getContract(contractAddress);
-    return contract.send('withdrawEscrow', {
-      methodArgs: [],
-      senderAddress,
-    });
   },
 
-  async totalQtumValue(args) {
-    const {
-      contractAddress, // address
-      senderAddress, // address
-    } = args;
+  async currentRound(args) {
+    try {
+      const { eventAddress } = args;
+      if (isUndefined(eventAddress)) throw TypeError('eventAddress is not defined');
 
-    if (_.isUndefined(contractAddress)) {
-      throw new TypeError('contractAddress needs to be defined');
+      const res = await getContract(eventAddress).methods.currentRound().call();
+      return res.toNumber();
+    } catch (err) {
+      getLogger().error(`Error MultipleResultsEvent.currentRound(): ${err.message}`);
+      throw err;
     }
-    if (_.isUndefined(senderAddress)) {
-      throw new TypeError('senderAddress needs to be defined');
-    }
-
-    const contract = getContract(contractAddress);
-    const res = await contract.call('totalQtumValue', {
-      methodArgs: [],
-      senderAddress,
-    });
-    res[0] = Utils.hexToDecimalString(res[0]);
-    return res;
   },
 
-  async totalBotValue(args) {
-    const {
-      contractAddress, // address
-      senderAddress, // address
-    } = args;
+  async currentResultIndex(args) {
+    try {
+      const { eventAddress } = args;
+      if (isUndefined(eventAddress)) throw TypeError('eventAddress is not defined');
 
-    if (_.isUndefined(contractAddress)) {
-      throw new TypeError('contractAddress needs to be defined');
+      const res = await getContract(eventAddress).methods.currentResultIndex().call();
+      return res.toNumber();
+    } catch (err) {
+      getLogger().error(`Error MultipleResultsEvent.currentResultIndex(): ${err.message}`);
+      throw err;
     }
-    if (_.isUndefined(senderAddress)) {
-      throw new TypeError('senderAddress needs to be defined');
-    }
-
-    const contract = getContract(contractAddress);
-    const res = await contract.call('totalBotValue', {
-      methodArgs: [],
-      senderAddress,
-    });
-    res[0] = Utils.hexToDecimalString(res[0]);
-    return res;
   },
 
-  async getFinalResult(args) {
-    const {
-      contractAddress, // address
-      senderAddress, // address
-    } = args;
+  async currentConsensusThreshold(args) {
+    try {
+      const { eventAddress } = args;
+      if (isUndefined(eventAddress)) throw TypeError('eventAddress is not defined');
 
-    if (_.isUndefined(contractAddress)) {
-      throw new TypeError('contractAddress needs to be defined');
+      const res = await getContract(eventAddress).methods.currentConsensusThreshold().call();
+      return res.toString(10);
+    } catch (err) {
+      getLogger().error(`Error MultipleResultsEvent.currentConsensusThreshold(): ${err.message}`);
+      throw err;
     }
-    if (_.isUndefined(senderAddress)) {
-      throw new TypeError('senderAddress needs to be defined');
-    }
-
-    const contract = getContract(contractAddress);
-    const res = await contract.call('getFinalResult', {
-      methodArgs: [],
-      senderAddress,
-    });
-    res[0] = Utils.hexToDecimalString(res[0]);
-    return res;
   },
 
-  async status(args) {
-    const {
-      contractAddress, // address
-      senderAddress, // address
-    } = args;
+  async currentArbitrationEndTime(args) {
+    try {
+      const { eventAddress } = args;
+      if (isUndefined(eventAddress)) throw TypeError('eventAddress is not defined');
 
-    if (_.isUndefined(contractAddress)) {
-      throw new TypeError('contractAddress needs to be defined');
+      const res = await getContract(eventAddress).methods.currentArbitrationEndTime().call();
+      return res.toString(10);
+    } catch (err) {
+      getLogger().error(`Error MultipleResultsEvent.currentArbitrationEndTime(): ${err.message}`);
+      throw err;
     }
-    if (_.isUndefined(senderAddress)) {
-      throw new TypeError('senderAddress needs to be defined');
-    }
+  },
 
-    const contract = getContract(contractAddress);
-    const res = await contract.call('status', {
-      methodArgs: [],
-      senderAddress,
-    });
-    res[0] = Utils.hexToDecimalString(res[0]);
-    return res;
+  async eventMetadata(args) {
+    try {
+      const { eventAddress } = args;
+      if (isUndefined(eventAddress)) throw TypeError('eventAddress is not defined');
+
+      const res = await getContract(eventAddress).methods.eventMetadata().call();
+      return [
+        res[0].toNumber(),
+        res[1],
+        map(res[2], item => web3().utils.toAscii(item)),
+        res[3].toNumber(),
+      ];
+    } catch (err) {
+      getLogger().error(`Error MultipleResultsEvent.eventMetadata(): ${err.message}`);
+      throw err;
+    }
+  },
+
+  async centralizedMetadata(args) {
+    try {
+      const { eventAddress } = args;
+      if (isUndefined(eventAddress)) throw TypeError('eventAddress is not defined');
+
+      const res = await getContract(eventAddress).methods.centralizedMetadata().call();
+      return [
+        res[0],
+        res[1].toString(10),
+        res[2].toString(10),
+        res[3].toString(10),
+        res[4].toString(10),
+      ];
+    } catch (err) {
+      getLogger().error(`Error MultipleResultsEvent.centralizedMetadata(): ${err.message}`);
+      throw err;
+    }
+  },
+
+  async configMetadata(args) {
+    try {
+      const { eventAddress } = args;
+      if (isUndefined(eventAddress)) throw TypeError('eventAddress is not defined');
+
+      const res = await getContract(eventAddress).methods.configMetadata().call();
+      return [
+        res[0].toString(10),
+        res[1].toString(10),
+        res[2].toString(10),
+        res[3].toString(10),
+      ];
+    } catch (err) {
+      getLogger().error(`Error MultipleResultsEvent.configMetadata(): ${err.message}`);
+      throw err;
+    }
+  },
+
+  async totalBets(args) {
+    try {
+      const { eventAddress } = args;
+      if (isUndefined(eventAddress)) throw TypeError('eventAddress is not defined');
+
+      const res = await getContract(eventAddress).methods.totalBets().call();
+      return res.toString(10);
+    } catch (err) {
+      getLogger().error(`Error MultipleResultsEvent.totalBets(): ${err.message}`);
+      throw err;
+    }
   },
 
   async didWithdraw(args) {
-    const {
-      contractAddress, // address
-      address, // address
-      senderAddress, // address
-    } = args;
+    try {
+      const { eventAddress, address } = args;
+      if (isUndefined(eventAddress)) throw TypeError('eventAddress is not defined');
+      if (isUndefined(address)) throw TypeError('address is not defined');
 
-    if (_.isUndefined(contractAddress)) {
-      throw new TypeError('contractAddress needs to be defined');
+      const res = await getContract(eventAddress).methods.didWithdraw(address).call();
+      return res;
+    } catch (err) {
+      getLogger().error(`Error MultipleResultsEvent.didWithdraw(): ${err.message}`);
+      throw err;
     }
-    if (_.isUndefined(address)) {
-      throw new TypeError('address needs to be defined');
-    }
-    if (_.isUndefined(senderAddress)) {
-      throw new TypeError('senderAddress needs to be defined');
-    }
-
-    const contract = getContract(contractAddress);
-    return contract.call('didWithdraw', {
-      methodArgs: [address],
-      senderAddress,
-    });
   },
 
-  async calculateWinnings(args) {
-    const {
-      contractAddress, // address
-      senderAddress, // address
-    } = args;
+  async didWithdrawEscrow(args) {
+    try {
+      const { eventAddress, address } = args;
+      if (isUndefined(eventAddress)) throw TypeError('eventAddress is not defined');
+      if (isUndefined(address)) throw TypeError('address is not defined');
 
-    if (_.isUndefined(contractAddress)) {
-      throw new TypeError('contractAddress needs to be defined');
+      const res = await getContract(eventAddress).methods.didWithdrawEscrow(address).call();
+      return res;
+    } catch (err) {
+      getLogger().error(`Error MultipleResultsEvent.didWithdrawEscrow(): ${err.message}`);
+      throw err;
     }
-    if (_.isUndefined(senderAddress)) {
-      throw new TypeError('senderAddress needs to be defined');
-    }
-
-    const contract = getContract(contractAddress);
-    const res = await contract.call('calculateWinnings', {
-      methodArgs: [],
-      senderAddress,
-    });
-    res[0] = Utils.hexToDecimalString(res[0]);
-    res[1] = Utils.hexToDecimalString(res[1]);
-    return res;
   },
 };
-
-module.exports = TopicEvent;
