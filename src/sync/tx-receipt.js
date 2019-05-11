888788fb72a4839473798cb0fe05d78bd98d78e2
Deric Walintukan <deric.walintukan@gmail.com>
2019-05-11 13:44:39 +0700

    Finish tx receipt logic

--- src/sync/tx-receipt.js
+++ src/sync/tx-receipt.js
@@ -1,64 +1,17 @@
-const { isNull, each } = require('lodash');
+const { isNull } = require('lodash');
 const { web3 } = require('../web3');
-const { getAbiObject } = require('../utils');
-const { getLogger } = require('../utils/logger');
-const Bet = require('../models/bet');
+const TransactionReceipt = require('../models/tx-receipt');
 const { db } = require('../db');
 const DBHelper = require('../db/db-helper');
 
 module.exports = async (txid) => {
   try {
-    const naka = web3();
+    const res = await web3().eth.getTransactionReceipt(txid);
+    if (isNull(res)) throw Error(`txid ${txid} does not have tx receipt`);
 
-    const receipt = await web3.eth.getTransactionReceipt(txid);
-    if (isNull(receipt)) throw Error(`txid ${txid} does not have tx receipt`);
-
-    
-
-
-    // Get all logs of event
-    const eventSig = naka.eth.abi.encodeEventSignature(obj);
-    const logs = await naka.eth.getPastLogs({
-      fromBlock: currentBlockNum,
-      toBlock: currentBlockNum,
-      topics: [eventSig],
-    });
-
-    // Parse each log
-    const promises = [];
-    each(logs, async (log) => {
-      const {
-        eventAddress,
-        better,
-        resultIndex,
-        amount,
-        eventRound,
-      } = naka.eth.abi.decodeLog(obj.inputs, log.data, log.topics);
-
-      const bet = new Bet({
-        blockNum: log.blockNumber,
-        txid: log.transactionHash,
-        eventAddress,
-        betterAddress: better,
-        resultIndex,
-        amount,
-        eventRound,
-      });
-
-      // Insert/update
-      promises.push(new Promise(async (resolve, reject) => {
-        try {
-          await DBHelper.insertBet(db, bet);
-          resolve();
-        } catch (insertErr) {
-          getLogger().error(`insert Bet: ${insertErr.message}`);
-          reject(insertErr);
-        }
-      }));
-    });
-
-    await Promise.all(promises);
+    const txReceipt = new TransactionReceipt(res);
+    await DBHelper.insertTxReceipt(db, txReceipt);
   } catch (err) {
-    throw Error('Error syncBetPlaced:', err);
+    throw Error('Error getTransactionReceipt:', err);
   }
 };
