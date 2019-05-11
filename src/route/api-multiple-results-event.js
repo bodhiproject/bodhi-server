ce03956260002503511284b26e5a1c502355f1e6
Deric Walintukan <deric.walintukan@gmail.com>
2019-05-11 22:24:21 +0700

    Move mr event api routes

---
+++ src/route/api-multiple-results-event.js
@@ -0,0 +1,139 @@
+const { Router } = require('express');
+const { onRequestSuccess, onRequestError, validateQueryParams } = require('./utils');
+const MultipleResultsEventApi = require('../api/multiple-results-event');
+
+const router = Router();
+
+router.get('/calculate-winnings', (req, res, next) => {
+  if (!validateQueryParams(['eventAddress', 'address'])) return;
+
+  MultipleResultsEventApi.calculateWinnings(req.query)
+    .then((result) => {
+      onRequestSuccess(res, result, next);
+    }, (err) => {
+      onRequestError(res, err, next);
+    });
+});
+
+router.get('/version', (req, res, next) => {
+  if (!validateQueryParams(['eventAddress'])) return;
+
+  MultipleResultsEventApi.version(req.query)
+    .then((result) => {
+      onRequestSuccess(res, result, next);
+    }, (err) => {
+      onRequestError(res, err, next);
+    });
+});
+
+router.get('/round', (req, res, next) => {
+  if (!validateQueryParams(['eventAddress'])) return;
+
+  MultipleResultsEventApi.currentRound(req.query)
+    .then((result) => {
+      onRequestSuccess(res, result, next);
+    }, (err) => {
+      onRequestError(res, err, next);
+    });
+});
+
+router.get('/result-index', (req, res, next) => {
+  if (!validateQueryParams(['eventAddress'])) return;
+
+  MultipleResultsEventApi.currentResultIndex(req.query)
+    .then((result) => {
+      onRequestSuccess(res, result, next);
+    }, (err) => {
+      onRequestError(res, err, next);
+    });
+});
+
+router.get('/consensus-threshold', (req, res, next) => {
+  if (!validateQueryParams(['eventAddress'])) return;
+
+  MultipleResultsEventApi.currentConsensusThreshold(req.query)
+    .then((result) => {
+      onRequestSuccess(res, result, next);
+    }, (err) => {
+      onRequestError(res, err, next);
+    });
+});
+
+router.get('/arbitration-end-time', (req, res, next) => {
+  if (!validateQueryParams(['eventAddress'])) return;
+
+  MultipleResultsEventApi.currentArbitrationEndTime(req.query)
+    .then((result) => {
+      onRequestSuccess(res, result, next);
+    }, (err) => {
+      onRequestError(res, err, next);
+    });
+});
+
+router.get('/event-metadata', (req, res, next) => {
+  if (!validateQueryParams(['eventAddress'])) return;
+
+  MultipleResultsEventApi.eventMetadata(req.query)
+    .then((result) => {
+      onRequestSuccess(res, result, next);
+    }, (err) => {
+      onRequestError(res, err, next);
+    });
+});
+
+router.get('/centralized-metadata', (req, res, next) => {
+  if (!validateQueryParams(['eventAddress'])) return;
+
+  MultipleResultsEventApi.centralizedMetadata(req.query)
+    .then((result) => {
+      onRequestSuccess(res, result, next);
+    }, (err) => {
+      onRequestError(res, err, next);
+    });
+});
+
+router.get('/config-metadata', (req, res, next) => {
+  if (!validateQueryParams(['eventAddress'])) return;
+
+  MultipleResultsEventApi.configMetadata(req.query)
+    .then((result) => {
+      onRequestSuccess(res, result, next);
+    }, (err) => {
+      onRequestError(res, err, next);
+    });
+});
+
+router.get('/total-bets', (req, res, next) => {
+  if (!validateQueryParams(['eventAddress'])) return;
+
+  MultipleResultsEventApi.totalBets(req.query)
+    .then((result) => {
+      onRequestSuccess(res, result, next);
+    }, (err) => {
+      onRequestError(res, err, next);
+    });
+});
+
+router.get('/did-withdraw', (req, res, next) => {
+  if (!validateQueryParams(['eventAddress', 'address'])) return;
+
+  MultipleResultsEventApi.didWithdraw(req.query)
+    .then((result) => {
+      onRequestSuccess(res, result, next);
+    }, (err) => {
+      onRequestError(res, err, next);
+    });
+});
+
+router.get('/did-withdraw-escrow', (req, res, next) => {
+  if (!validateQueryParams(['eventAddress', 'address'])) return;
+
+  MultipleResultsEventApi.didWithdrawEscrow(req.query)
+    .then((result) => {
+      onRequestSuccess(res, result, next);
+    }, (err) => {
+      onRequestError(res, err, next);
+    });
+});
+
+module.exports = router;
