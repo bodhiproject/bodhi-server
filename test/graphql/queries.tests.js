f092abe2119fc10ad6cfadab0684c029fea6bc47
Deric Walintukan <deric.walintukan@gmail.com>
2019-05-21 23:55:50 +0700

    Add events tests

--- test/graphql/queries.tests.js
+++ test/graphql/queries.tests.js
@@ -1,11 +1,167 @@
 const EasyGraphQLTester = require('easygraphql-tester');
-const { assert } = require('chai');
-const { PAGINATED_BETS } = require('./schema-helper');
+const { PAGINATED_EVENTS, PAGINATED_BETS } = require('./schema-helper');
 const schema = require('../../src/graphql/schema');
 
 const tester = new EasyGraphQLTester(schema);
 
 describe('queries', () => {
+  describe('events', () => {
+    it('should return the query', async () => {
+      const valid = `
+        query {
+          events {
+            ${PAGINATED_EVENTS}
+          }
+        }
+      `;
+      tester.test(true, valid);
+    });
+
+    it('should accept the OR filter', async () => {
+      const valid = `
+        query {
+          events(filter: { OR: [
+            { txid: "0xd5d087daabc73fc6cc5d9c1131b93acbd53a2428" }
+            { txStatus: SUCCESS }
+            { address: "0xd5d087daabc73fc6cc5d9c1131b93acbd53a2428" }
+            { ownerAddress: "0xd5d087daabc73fc6cc5d9c1131b93acbd53a2428" }
+            { version: 0 }
+            { centralizedOracle: "0xd5d087daabc73fc6cc5d9c1131b93acbd53a2428" }
+            { currentRound: 0 }
+            { currentResultIndex: 0 }
+            { status: BETTING }
+            { language: "en-US" }
+            { excludeCentralizedOracle: "0xd5d087daabc73fc6cc5d9c1131b93acbd53a2428" }
+          ] }) {
+            ${PAGINATED_EVENTS}
+          }
+        }
+      `;
+      tester.test(true, valid);
+    });
+
+    it('should accept the txid filter', async () => {
+      const valid = `
+        query {
+          events(filter: { txid: "0xd5d087daabc73fc6cc5d9c1131b93acbd53a2428" }) {
+            ${PAGINATED_EVENTS}
+          }
+        }
+      `;
+      tester.test(true, valid);
+    });
+
+    it('should accept the txStatus filter', async () => {
+      const valid = `
+        query {
+          events(filter: { txStatus: SUCCESS }) {
+            ${PAGINATED_EVENTS}
+          }
+        }
+      `;
+      tester.test(true, valid);
+    });
+
+    it('should accept the address filter', async () => {
+      const valid = `
+        query {
+          events(filter: { address: "0xd5d087daabc73fc6cc5d9c1131b93acbd53a2428" }) {
+            ${PAGINATED_EVENTS}
+          }
+        }
+      `;
+      tester.test(true, valid);
+    });
+
+    it('should accept the ownerAddress filter', async () => {
+      const valid = `
+        query {
+          events(filter: { ownerAddress: "0xd5d087daabc73fc6cc5d9c1131b93acbd53a2428" }) {
+            ${PAGINATED_EVENTS}
+          }
+        }
+      `;
+      tester.test(true, valid);
+    });
+
+    it('should accept the version filter', async () => {
+      const valid = `
+        query {
+          events(filter: { version: 0 }) {
+            ${PAGINATED_EVENTS}
+          }
+        }
+      `;
+      tester.test(true, valid);
+    });
+
+    it('should accept the centralizedOracle filter', async () => {
+      const valid = `
+        query {
+          events(filter: { centralizedOracle: "0xd5d087daabc73fc6cc5d9c1131b93acbd53a2428" }) {
+            ${PAGINATED_EVENTS}
+          }
+        }
+      `;
+      tester.test(true, valid);
+    });
+
+    it('should accept the currentRound filter', async () => {
+      const valid = `
+        query {
+          events(filter: { currentRound: 0 }) {
+            ${PAGINATED_EVENTS}
+          }
+        }
+      `;
+      tester.test(true, valid);
+    });
+
+    it('should accept the currentResultIndex filter', async () => {
+      const valid = `
+        query {
+          events(filter: { currentResultIndex: 0 }) {
+            ${PAGINATED_EVENTS}
+          }
+        }
+      `;
+      tester.test(true, valid);
+    });
+
+    it('should accept the status filter', async () => {
+      const valid = `
+        query {
+          events(filter: { status: BETTING }) {
+            ${PAGINATED_EVENTS}
+          }
+        }
+      `;
+      tester.test(true, valid);
+    });
+
+    it('should accept the language filter', async () => {
+      const valid = `
+        query {
+          events(filter: { language: "en-US" }) {
+            ${PAGINATED_EVENTS}
+          }
+        }
+      `;
+      tester.test(true, valid);
+    });
+
+    it('should accept the excludeCentralizedOracle filter', async () => {
+      const valid = `
+        query {
+          events(filter: { excludeCentralizedOracle: "0xd5d087daabc73fc6cc5d9c1131b93acbd53a2428" }) {
+            ${PAGINATED_EVENTS}
+          }
+        }
+      `;
+      tester.test(true, valid);
+    });
+  });
+
   describe('bets', () => {
     it('should return the query', async () => {
       const valid = `
--- test/graphql/schema-helper.js
+++ test/graphql/schema-helper.js
@@ -28,6 +28,48 @@ const TX_RECEIPT = `
   }
 `;
 
+const PAGINATED_EVENTS = `
+  totalCount
+  ${PAGE_INFO}
+  items {
+    txType
+    txid
+    txStatus
+    ${TX_RECEIPT}
+    blockNum
+    ${BLOCK}
+    address
+    ownerAddress
+    version
+    name
+    results
+    numOfResults
+    centralizedOracle
+    betStartTime
+    betEndTime
+    resultSetStartTime
+    resultSetEndTime
+    escrowAmount
+    arbitrationLength
+    thresholdPercentIncrease
+    arbitrationRewardPercentage
+    currentRound
+    currentResultIndex
+    consensusThreshold
+    arbitrationEndTime
+    status
+    language
+    pendingTxs {
+      bet
+      resultSet
+      withdraw
+      total
+    }
+    roundBets
+    totalBets
+  }
+`;
+
 const PAGINATED_BETS = `
   totalCount
   ${PAGE_INFO}
@@ -51,5 +93,6 @@ module.exports = {
   PAGE_INFO,
   BLOCK,
   TX_RECEIPT,
+  PAGINATED_EVENTS,
   PAGINATED_BETS,
 };
