a4f90915f4681fc805fd40f3c63dbc3ccfdd98cf
Deric Walintukan <deric.walintukan@gmail.com>
2019-05-29 19:18:21 +0700

    Handle event does not exist in update event

--- src/sync/update-event.js
+++ src/sync/update-event.js
@@ -1,4 +1,6 @@
+const { isNull } = require('lodash');
 const DBHelper = require('../db/db-helper');
+const logger = require('../utils/logger');
 
 /**
  * Updates the event if the result set is the latest one.
@@ -6,6 +8,13 @@ const DBHelper = require('../db/db-helper');
  */
 module.exports = async (resultSet) => {
   const event = await DBHelper.findOneEvent({ address: resultSet.eventAddress });
+
+  // Log error if event does not exist
+  if (isNull(event)) {
+    logger.error(`Error updateEvent ${resultSet.eventAddress}: Event does not exist`);
+    return;
+  }
+
   if (event.currentRound < resultSet.eventRound + 1) {
     event.currentRound = resultSet.eventRound + 1;
     event.currentResultIndex = resultSet.resultIndex;
