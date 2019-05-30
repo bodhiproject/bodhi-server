4ac88f9fce21215b88e7432342790879d35c0e3f
Deric Walintukan <deric.walintukan@gmail.com>
2019-05-30 12:20:48 +0700

    Fix missing contract address for event factory

--- src/config/contract-metadata.js
+++ src/config/contract-metadata.js
@@ -16,7 +16,7 @@ module.exports = {
     EventFactory: {
       mainnet: '',
       mainnetDeployBlock: 0,
-      testnet: '',
+      testnet: '0x20198bff1bf63e011417c4dd1c3f0c8768065adb',
       testnetDeployBlock: 3612779,
       abi: [{"inputs": [{"name": "configManager","type": "address"}],"payable": false,"stateMutability": "nonpayable","type": "constructor"},{"anonymous": false,"inputs": [{"indexed": true,"name": "eventAddress","type": "address"},{"indexed": true,"name": "ownerAddress","type": "address"}],"name": "MultipleResultsEventCreated","type": "event"},{"constant": false,"inputs": [{"name": "from","type": "address"},{"name": "value","type": "uint256"},{"name": "data","type": "bytes"}],"name": "tokenFallback","outputs": [],"payable": false,"stateMutability": "nonpayable","type": "function"},{"constant": false,"inputs": [],"name": "withdrawEscrow","outputs": [{"name": "","type": "uint256"}],"payable": false,"stateMutability": "nonpayable","type": "function"},{"constant": true,"inputs": [],"name": "didWithdraw","outputs": [{"name": "","type": "bool"}],"payable": false,"stateMutability": "view","type": "function"}],
     },
