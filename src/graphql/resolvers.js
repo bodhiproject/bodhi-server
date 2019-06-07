8a033dc6395e20ddccf7a7b63fd1f5d1f464aaa5
Bowen Zhang <bowen@bohdi.network>
2019-06-07 14:57:20 -0700

    Show consensus threshold

--- src/graphql/resolvers.js
+++ src/graphql/resolvers.js
@@ -131,6 +131,23 @@ module.exports = {
       }
       return null;
     },
+    previousConsensusThreshold: async (
+      { address, currentRound },
+      args,
+      {},
+    ) => {
+        // Add result set amount if round 1
+        if (currentRound >= 1) {
+          const resultSet = await DBHelper.findOneResultSet({
+            txStatus: TX_STATUS.SUCCESS,
+            eventAddress: address,
+            eventRound: currentRound - 1,
+          });
+          return resultSet.amount
+
+        }
+        return null
+    },
     totalBets: async ({ address }) => {
       const bets = await DBHelper.findBet({
         txStatus: TX_STATUS.SUCCESS,
