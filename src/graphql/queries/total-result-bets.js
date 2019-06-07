13194a8007d21cfe40b8e6c007913ec38a01a3fb
Bowen Zhang <bowen@bohdi.network>
2019-06-07 13:51:31 -0700

    Query only successful txs

--- src/graphql/queries/total-result-bets.js
+++ src/graphql/queries/total-result-bets.js
@@ -2,6 +2,7 @@ const { isNull, fill, each, map } = require('lodash');
 const { lowercaseFilters } = require('./utils');
 const DBHelper = require('../../db/db-helper');
 const web3 = require('../../web3');
+const { TX_STATUS } = require('../../constants');
 const { toLowerCase } = require('../../utils/index');
 
 const accumulateBets = (numOfResults, bets) => {
@@ -29,22 +30,22 @@ module.exports = async (
   const { numOfResults } = event;
 
   // Accumulate all round 0 bets
-  let totalBetRound = await DBHelper.findBet({ eventAddress, eventRound: 0 });
+  let totalBetRound = await DBHelper.findBet({ eventAddress, eventRound: 0, txStatus: TX_STATUS.SUCCESS});
   const totalBets = accumulateBets(numOfResults, totalBetRound);
 
   // Accumulate better round 0 bets
   let betterBets
   if (betterAddress) {
-    betterBetRound = await DBHelper.findBet({ eventAddress, eventRound: 0, betterAddress });
+    betterBetRound = await DBHelper.findBet({ eventAddress, eventRound: 0, betterAddress, txStatus: TX_STATUS.SUCCESS });
     betterBets = accumulateBets(numOfResults, betterBetRound);
   }
 
   // Get result set
-  const resultSet = await DBHelper.findOneResultSet({ eventAddress, eventRound: 0 });
+  const resultSet = await DBHelper.findOneResultSet({ eventAddress, eventRound: 0, txStatus: TX_STATUS.SUCCESS });
   const { centralizedOracleAddress } = resultSet;
 
   // Accumulate all result bets
-  let totalVoteRound = await DBHelper.findBet({ eventAddress, eventRound: { $gt: 0 } } );
+  let totalVoteRound = await DBHelper.findBet({ eventAddress, eventRound: { $gt: 0 }, txStatus: TX_STATUS.SUCCESS } );
   totalVoteRound = totalVoteRound.concat(resultSet);
 
   // Add result set amount
@@ -53,7 +54,7 @@ module.exports = async (
   // Accumulate all better bets
   let betterVotes;
   if (betterAddress) {
-    betterVoteRound = await DBHelper.findBet({ eventAddress, betterAddress, eventRound: { $gt: 0 } });
+    betterVoteRound = await DBHelper.findBet({ eventAddress, betterAddress, eventRound: { $gt: 0 }, txStatus: TX_STATUS.SUCCESS });
     if (toLowerCase(betterAddress) === centralizedOracleAddress) {
       betterVoteRound = betterVoteRound.concat(resultSet);
     }
