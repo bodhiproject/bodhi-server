a1097096e8f538a8ceb5637a15375ba15da1826e
Deric Walintukan <deric.walintukan@gmail.com>
2019-06-19 15:35:16 +0700

    Add route for api log

---
+++ src/route/api-log.js
@@ -0,0 +1,16 @@
+const { Router } = require('express');
+const { onRequestSuccess, onRequestError } = require('./utils');
+const LogApi = require('../api/log');
+
+const router = Router();
+
+router.post('/log-client-error', (req, res, next) => {
+  LogApi.logClientError(req.body)
+    .then((result) => {
+      onRequestSuccess(res, result, next);
+    }, (err) => {
+      onRequestError(res, err, next);
+    });
+});
+
+module.exports = router;
