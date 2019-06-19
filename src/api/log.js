7086fb54bd6e665fb5f022567f5738e0ebe7702c
Deric Walintukan <deric.walintukan@gmail.com>
2019-06-19 15:15:18 +0700

    Finish client log api

--- src/api/log.js
+++ src/api/log.js
@@ -1,12 +1,19 @@
-
+const clientLogger = require('../utils/client-logger');
 
 module.exports = {
+  /**
+   * POST request to log client errors.
+   * @param {object} args Arguments including log level and log message.
+   */
   async logClientError(args) {
-    try {
-      return getContract().methods.bodhiTokenAddress().call();
-    } catch (err) {
-      logger.error(`Error ConfigManager.bodhiTokenAddress(): ${err.message}`);
-      throw err;
+    const { level, message } = args;
+    if (!level) throw Error('level is not defined');
+    if (!message) throw Error('message is not defined');
+
+    if (level !== 'error') {
+      throw Error('logClientError only accepts log level error');
     }
+
+    clientLogger.error(message);
   },
 };
