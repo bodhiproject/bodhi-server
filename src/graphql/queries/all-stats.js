b31961f5b8cdd60c0365de3c0b4bfc819a77a94b
Bernard <bernardhan221@gmail.com>
2019-06-19 17:06:39 -0700

    Fix all-stat

--- src/graphql/queries/all-stats.js
+++ src/graphql/queries/all-stats.js
@@ -1,13 +1,19 @@
 const { uniqBy, each } = require('lodash');
 const web3 = require('../../web3');
+const { TX_STATUS } = require('../../constants');
 
 module.exports = async (
   root,
   {}, // eslint-disable-line no-empty-pattern
-  { db: { Events, Bets } },
+  { db: { Events, Bets, ResultSets } },
 ) => {
   const eventCount = Events.count({});
-  const bets = await Bets.find({});
+  let bets = await Bets.find({});
+  const resultsets = await ResultSets.find({
+    txStatus: TX_STATUS.SUCCESS,
+    eventRound: 0,
+  });
+  bets = bets.concat(resultsets);
   const participantCount = uniqBy(bets, 'betterAddress').length;
 
   let totalBets = web3.utils.toBN(0);
