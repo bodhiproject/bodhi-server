bba1e97cf2eae992b54d3dadb891e145eaf2b3cf
Deric Walintukan <deric.walintukan@gmail.com>
2019-06-24 12:32:50 +0700

    Add reconnect logic

--- src/web3.js
+++ src/web3.js
@@ -4,32 +4,48 @@ const { BLOCKCHAIN_ENV, EVENT_MESSAGE } = require('./constants');
 const logger = require('./utils/logger');
 const emitter = require('./event');
 
-// Get endpoint per network
-let url;
-let msg;
-if (CONFIG.NETWORK === BLOCKCHAIN_ENV.MAINNET) {
-  url = CONFIG.WS_PROVIDER_MAINNET;
-  msg = 'Web3 connected to Mainnet';
-} else {
-  url = CONFIG.WS_PROVIDER_TESTNET;
-  msg = 'Web3 connected to Testnet';
-}
+const web3 = new Web3(getProvider());
 
-// Create provider and handle events
-const provider = new Web3.providers.WebsocketProvider(url);
-provider.on('connect', () => {
-  logger.info(msg);
-  emitter.emit(EVENT_MESSAGE.WEBSOCKET_CONNECTED);
-});
-provider.on('error', (err) => {
-  logger.error(`Web3 WS encountered an error: ${err.message}`);
-  emitter.emit(EVENT_MESSAGE.WEBSOCKET_DISCONNECTED);
-  throw err;
-});
-provider.on('end', (err) => {
-  logger.error('Web3 WS disconnected');
-  emitter.emit(EVENT_MESSAGE.WEBSOCKET_DISCONNECTED);
-  throw err;
-});
+/**
+ * Returns the websocket provider with implemented event handlers.
+ * Event handlers will create a new provider and set it to the existing web3
+ * instance when a disconnect occurs.
+ * @return {Web3.providers.WebsocketProvider} Web3 WS Provider.
+ */
+const getProvider = () => {
+  // Get endpoint per network
+  let url;
+  let msg;
+  if (CONFIG.NETWORK === BLOCKCHAIN_ENV.MAINNET) {
+    url = CONFIG.WS_PROVIDER_MAINNET;
+    msg = 'Web3 connected to Mainnet';
+  } else {
+    url = CONFIG.WS_PROVIDER_TESTNET;
+    msg = 'Web3 connected to Testnet';
+  }
 
-module.exports = new Web3(provider);
+  // Create provider and handle events
+  const provider = new Web3.providers.WebsocketProvider(url);
+  provider.on('connect', () => {
+    logger.info(msg);
+    emitter.emit(EVENT_MESSAGE.WEBSOCKET_CONNECTED);
+  });
+  provider.on('error', (err) => {
+    logger.error(`Web3 WS encountered an error: ${err.message}`);
+    emitter.emit(EVENT_MESSAGE.WEBSOCKET_DISCONNECTED);
+
+    // Handle ws disconnects and create new instance
+    web3.setProvider(getProvider());
+  });
+  provider.on('end', (err) => {
+    logger.error(`Web3 WS disconnected: ${err.message}`);
+    emitter.emit(EVENT_MESSAGE.WEBSOCKET_DISCONNECTED);
+
+    // Handle ws disconnects and create new instance
+    web3.setProvider(getProvider());
+  });
+
+  return provider;
+};
+
+module.exports = web3;
