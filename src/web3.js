980c090b20fd0a5b7b8bbf912b0b2ae136ed6270
Deric Walintukan <deric.walintukan@gmail.com>
2019-06-24 11:28:30 +0700

    Add event handler for ws provider

--- src/web3.js
+++ src/web3.js
@@ -3,13 +3,27 @@ const { CONFIG } = require('./config');
 const { BLOCKCHAIN_ENV } = require('./constants');
 const logger = require('./utils/logger');
 
+// Get endpoint per network
 let url;
+let msg;
 if (CONFIG.NETWORK === BLOCKCHAIN_ENV.MAINNET) {
   url = CONFIG.WS_PROVIDER_MAINNET;
-  logger.info('Web3 connected to Mainnet');
+  msg = 'Web3 connected to Mainnet';
 } else {
   url = CONFIG.WS_PROVIDER_TESTNET;
-  logger.info('Web3 connected to Testnet');
+  msg = 'Web3 connected to Testnet';
 }
 
-module.exports = new Web3(url);
+// Create provider and handle events
+const provider = new Web3.providers.WebsocketProvider(url);
+provider.on('connect', () => logger.info(msg));
+provider.on('error', (err) => {
+  logger.error(`Web3 WS encountered an error: ${err.message}`);
+  throw err;
+});
+provider.on('end', () => {
+  logger.error('Web3 WS disconnected');
+  throw Error('Web3 WS disconnected');
+});
+
+module.exports = new Web3(provider);
