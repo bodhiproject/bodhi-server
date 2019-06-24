facffaebd0324dad28971909dec569230b020636
Deric Walintukan <deric.walintukan@gmail.com>
2019-06-24 11:11:42 +0700

    Change config constant names

--- src/config/index.js
+++ src/config/index.js
@@ -8,8 +8,8 @@ const MultipleResultsEvent = require('./contracts/multiple-results-event');
 
 const CONFIG = {
   NETWORK: process.env.NETWORK,
-  RPC_MAINNET: 'wss://api.nakachain.org/ws',
-  RPC_TESTNET: 'wss://testnet.api.nakachain.org/ws',
+  WS_PROVIDER_MAINNET: 'wss://api.nakachain.org/ws',
+  WS_PROVIDER_TESTNET: 'wss://testnet.api.nakachain.org/ws',
   PROTOCOL: process.env.SSL === 'true' ? 'https' : 'http',
   HOSTNAME: 'localhost',
   API_PORT_MAINNET: 8888,
