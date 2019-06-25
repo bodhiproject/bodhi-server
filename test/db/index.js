2ad5f43b3986efe498c0f035663141390135ac3e
Bowen Zhang <bowen@bohdi.network>
2019-06-25 15:32:28 -0700

    Add remove after tests

--- test/db/index.js
+++ test/db/index.js
@@ -1,13 +1,21 @@
 const { assert } = require('chai');
+const fs = require('fs-extra'); 
 const { initDB } = require('../../src/db');
+const { getDbDir } = require('../../src/config');
 
 describe('db', () => {
   before(async () => {
     await initDB();
   })
 
-  
 
+  it('12', () => {
+    assert.isFalse(false);
+  });
+
+  after(async () => {
+    fs.removeSync(getDbDir())
+  })
 })
 
 
