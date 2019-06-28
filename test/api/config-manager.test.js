c8e93237f4338b401eb41e098c296683b7a844a0
Bernard <bernardhan221@gmail.com>
2019-06-28 15:24:29 -0700

    Add tests for config manager

--- test/api/config-manager.test.js
+++ test/api/config-manager.test.js
@@ -13,7 +13,7 @@ const expect = Chai.expect;
 const eventAddress = '0x8b2b1838efff78e5ed9e00d95d9ef071f2c27be6';
 const address = '0x939592864C0Bd3355B2D54e4fA2203E8343B6d6a';
 
-describe('api/multiple-results-event', () => {
+describe('api/config-manager', () => {
   let stubGetContract;
   beforeEach(() => {
       stubGetContract = sinon.stub(ConfigManager, 'getContract').callsFake(getContract);
@@ -23,134 +23,48 @@ describe('api/multiple-results-event', () => {
     stubGetContract.restore();
   });
 
-  describe('calculateWinnings()', () => {
-    it('It returns the calculateWinnings', async () => {
-      const res = await ConfigManager.calculateWinnings({ eventAddress, address });
+  describe('bodhiTokenAddress()', () => {
+    it('It returns the bodhiTokenAddress', async () => {
+      const res = await ConfigManager.bodhiTokenAddress();
+      assert.isDefined(res);
+      assert.isTrue(isString(res));
+    });
+  });
+
+  describe('eventFactoryAddress()', () => {
+    it('It returns the eventFactoryAddress', async () => {
+      const res = await ConfigManager.eventFactoryAddress();
+      assert.isDefined(res);
+      assert.isTrue(isString(res));
+    });
+  });
+
+  describe('eventEscrowAmount()', () => {
+    it('It returns the eventEscrowAmount', async () => {
+      const res = await ConfigManager.eventEscrowAmount();
       assert.isDefined(res);
       assert.isTrue(isString(res));
       assert.isFalse(isNaN(res));
     });
-
-    it('It throws if eventAddress is undefined', () => {
-      expect(ConfigManager.calculateWinnings({
-        address,
-      })).to.be.rejectedWith(Error);
-    });
-
-    it('It throws if address is undefined', () => {
-      expect(ConfigManager.calculateWinnings({
-        eventAddress,
-      })).to.be.rejectedWith(Error);
-    });
   });
 
-  describe('version()', () => {
-    it('It returns the version', async () => {
-      const res = await ConfigManager.version({ eventAddress });
-      assert.isDefined(res);
-      assert.isTrue(isNumber(res));
-    });
-
-    it('It throws if eventAddress is undefined', () => {
-      expect(ConfigManager.version({})).to.be.rejectedWith(Error);
-    });
-  });
-
-  describe('currentRound()', () => {
-    it('It returns the currentRound', async () => {
-      const res = await ConfigManager.currentRound({ eventAddress });
-      assert.isDefined(res);
-      assert.isTrue(isNumber(res));
-    });
-
-    it('It throws if eventAddress is undefined', () => {
-      expect(ConfigManager.currentRound({})).to.be.rejectedWith(Error);
-    });
-  });
-
-  describe('currentResultIndex()', () => {
-    it('It returns the currentResultIndex', async () => {
-      const res = await ConfigManager.currentResultIndex({ eventAddress });
-      assert.isDefined(res);
-      assert.isTrue(isNumber(res));
-    });
-
-    it('It throws if eventAddress is undefined', () => {
-      expect(ConfigManager.currentResultIndex({})).to.be.rejectedWith(Error);
-    });
-  });
-
-  describe('currentConsensusThreshold()', () => {
-    it('It returns the currentConsensusThreshold', async () => {
-      const res = await ConfigManager.currentConsensusThreshold({ eventAddress });
-      assert.isDefined(res);
-      assert.isTrue(isString(res));
-      assert.isFalse(isNaN(res));
-    });
-
-    it('It throws if eventAddress is undefined', () => {
-      expect(ConfigManager.currentConsensusThreshold({})).to.be.rejectedWith(Error);
-    });
-  });
-
-  describe('currentArbitrationEndTime()', () => {
-    it('It returns the currentArbitrationEndTime', async () => {
-      const res = await ConfigManager.currentArbitrationEndTime({ eventAddress });
-      assert.isDefined(res);
-      assert.isTrue(isString(res));
-      assert.isFalse(isNaN(res));
-    });
-
-    it('It throws if eventAddress is undefined', () => {
-      expect(ConfigManager.currentArbitrationEndTime({})).to.be.rejectedWith(Error);
-    });
-  });
-
-  describe('eventMetadata()', () => {
-    it('It returns the eventMetadata', async () => {
-      const res = await ConfigManager.eventMetadata({ eventAddress });
+  describe('arbitrationLength()', () => {
+    it('It returns the arbitrationLength', async () => {
+      const res = await ConfigManager.arbitrationLength();
       assert.isDefined(res);
       assert.isTrue(isArray(res));
       assert.isTrue(res.length === 4);
 
       assert.isTrue(isNumber(res[0]));
-      assert.isTrue(isString(res[1]));
-      assert.isTrue(isArray(res[2]));
+      assert.isTrue(isNumber(res[1]));
+      assert.isTrue(isNumber(res[2]));
       assert.isTrue(isNumber(res[3]));
-      assert.isTrue(res[2].length === res[3]);
-    });
-
-    it('It throws if eventAddress is undefined', () => {
-      expect(ConfigManager.eventMetadata({})).to.be.rejectedWith(Error);
     });
   });
 
-  describe('centralizedMetadata()', () => {
-    it('It returns the centralizedMetadata', async () => {
-      const res = await ConfigManager.centralizedMetadata({ eventAddress });
-      assert.isDefined(res);
-      assert.isTrue(isArray(res));
-      assert.isTrue(res.length === 5);
-
-      assert.isTrue(isString(res[0]));
-      assert.isTrue(isString(res[1]));
-      assert.isFalse(isNaN(res[1]));
-      assert.isTrue(isString(res[2]));
-      assert.isFalse(isNaN(res[2]));
-      assert.isTrue(isString(res[3]));
-      assert.isFalse(isNaN(res[3]));
-      assert.isTrue(isString(res[4]));
-      assert.isFalse(isNaN(res[4]));
-    });
-
-    it('It throws if eventAddress is undefined', () => {
-      expect(ConfigManager.centralizedMetadata({})).to.be.rejectedWith(Error);
-    });
-  });
-
-  describe('configMetadata()', () => {
-    it('It returns the configMetadata', async () => {
-      const res = await ConfigManager.configMetadata({ eventAddress });
+  describe('startingConsensusThreshold()', () => {
+    it('It returns the startingConsensusThreshold', async () => {
+      const res = await ConfigManager.startingConsensusThreshold();
       assert.isDefined(res);
       assert.isTrue(isArray(res));
       assert.isTrue(res.length === 4);
@@ -164,54 +78,26 @@ describe('api/multiple-results-event', () => {
       assert.isTrue(isString(res[3]));
       assert.isFalse(isNaN(res[3]));
     });
-
-    it('It throws if eventAddress is undefined', () => {
-      expect(ConfigManager.configMetadata({})).to.be.rejectedWith(Error);
-    });
   });
 
-  describe('totalBets()', () => {
-    it('It returns the totalBets', async () => {
-      const res = await ConfigManager.totalBets({ eventAddress, address });
+  describe('thresholdPercentIncrease()', () => {
+    it('It returns the thresholdPercentIncrease', async () => {
+      const res = await ConfigManager.thresholdPercentIncrease();
       assert.isDefined(res);
       assert.isTrue(isString(res));
       assert.isFalse(isNaN(res));
     });
-
-    it('It throws if eventAddress is undefined', () => {
-      expect(ConfigManager.totalBets({})).to.be.rejectedWith(Error);
-    });
   });
 
-  describe('didWithdraw()', () => {
-    it('It returns the didWithdraw', async () => {
-      const res = await ConfigManager.didWithdraw({ eventAddress, address });
+  describe('isWhitelisted()', () => {
+    it('It returns the isWhitelisted', async () => {
+      const res = await ConfigManager.isWhitelisted({ address });
       assert.isDefined(res);
       assert.isTrue(isBoolean(res));
-    });
-
-    it('It throws if eventAddress is undefined', () => {
-      expect(ConfigManager.didWithdraw({
-        address,
-      })).to.be.rejectedWith(Error);
     });
 
     it('It throws if address is undefined', () => {
-      expect(ConfigManager.didWithdraw({
-        eventAddress,
-      })).to.be.rejectedWith(Error);
-    });
-  });
-
-  describe('didWithdrawEscrow()', () => {
-    it('It returns the didWithdrawEscrow', async () => {
-      const res = await ConfigManager.didWithdrawEscrow({ eventAddress, address });
-      assert.isDefined(res);
-      assert.isTrue(isBoolean(res));
-    });
-
-    it('It throws if eventAddress is undefined', () => {
-      expect(ConfigManager.didWithdrawEscrow({})).to.be.rejectedWith(Error);
+      expect(ConfigManager.isWhitelisted()).to.be.rejectedWith(Error);
     });
   });
 });
