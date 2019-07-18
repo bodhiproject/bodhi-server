2a5f976fd073f51fcb88afcf99bdf423fef9276e
Deric Walintukan <deric.walintukan@gmail.com>
2019-07-19 00:34:41 +0800

    Add get prov for local func

--- src/web3.js
+++ src/web3.js
@@ -6,13 +6,10 @@ const logger = require('./utils/logger');
 const emitter = require('./event');
 
 /**
- * Returns the websocket provider with implemented event handlers.
- * Event handlers will create a new provider and set it to the existing web3
- * instance when a disconnect occurs.
- * @return {WebsocketProvider} Web3 WS Provider
+ * Returns the web3 provider.
+ * @return {IpcProvider} Web3 provider
  */
 const getProvider = () => {
-  // Get endpoint per network
   let uri;
   let msg;
   if (CONFIG.NETWORK === BLOCKCHAIN_ENV.MAINNET) {
@@ -22,24 +19,41 @@ const getProvider = () => {
     uri = CONFIG.IPC_PROVIDER_TESTNET;
     msg = 'Web3 connected to Testnet';
   }
+
   const provider = new Web3.providers.IpcProvider(uri, net);
   logger.info(msg);
+  return provider;
+};
 
-  // TODO: use websockets again when web3 fixes it
-  // Create provider and handle events
-  // const provider = new Web3.providers.WebsocketProvider(uri);
-  // provider.on('connect', () => {
-  //   logger.info(msg);
-  //   emitter.emit(EVENT_MESSAGE.WEBSOCKET_CONNECTED);
-  // });
-  // provider.on('close', (err) => {
-  //   logger.error('Web3 WS closed', { error: err && err.message });
-  //   emitter.emit(EVENT_MESSAGE.WEBSOCKET_DISCONNECTED);
-  // });
-  // provider.on('error', (err) => {
-  //   logger.error('Web3 WS error', { error: err && err.message });
-  // });
+/**
+ * Returns the web3 provider and sets up event handlers.
+ * Event handlers will create a new provider and set it to the existing web3
+ * instance when a disconnect occurs.
+ * @return {WebsocketProvider} Web3 provider
+ */
+const getProviderForLocal = () => {
+  let uri;
+  let msg;
+  if (CONFIG.NETWORK === BLOCKCHAIN_ENV.MAINNET) {
+    uri = CONFIG.WS_PROVIDER_MAINNET;
+    msg = 'Web3 connected to Mainnet';
+  } else {
+    uri = CONFIG.WS_PROVIDER_TESTNET;
+    msg = 'Web3 connected to Testnet';
+  }
 
+  const provider = new Web3.providers.WebsocketProvider(uri);
+  provider.on('connect', () => {
+    logger.info(msg);
+    emitter.emit(EVENT_MESSAGE.WEBSOCKET_CONNECTED);
+  });
+  provider.on('close', (err) => {
+    logger.error('Web3 WS closed', { error: err && err.message });
+    emitter.emit(EVENT_MESSAGE.WEBSOCKET_DISCONNECTED);
+  });
+  provider.on('error', (err) => {
+    logger.error('Web3 WS error', { error: err && err.message });
+  });
   return provider;
 };
 
