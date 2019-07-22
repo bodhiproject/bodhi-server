3e6bfe5a324bb3514f7569e0be0fc9f9fdcb48da
Bowen Zhang <bowen@bohdi.network>
2019-07-22 14:04:56 -0700

    Change to post

--- src/api/address-name-service.js
+++ src/api/address-name-service.js
@@ -14,11 +14,9 @@ module.exports = {
   async resolveAddress(address) {
     try {
       if (isUndefined(address)) throw TypeError('address is not defined');
-      const { data } = await axios.get(`${getNakaBaseUrl()}/resolve-address`, {
-        params: {
-          address,
-          apikey: CONFIG.NAKABASE_API_KEY,
-        },
+      const { data } = await axios.post(`${getNakaBaseUrl()}/resolve-address`, {
+        address,
+        apikey: CONFIG.API_KEY,
       });
       return data;
     } catch (err) {
